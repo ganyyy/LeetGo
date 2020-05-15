f86c5c3ef94a5fdc21e422aa82227e48baf77563
Gann <1354410847@qq.com>
2020-05-15 23:09:55 +0800

    自定义和

---
+++ 500-600/lc560_subarraySum.go
@@ -0,0 +1,24 @@
+package main
+
+func subarraySum(nums []int, k int) int {
+	m := make(map[int]int)
+	// 表示正好为0的情况
+	m[0] = 1
+	// 当前的累加和, 返回的结果值
+	var sum, ret int
+	// 遍历数组的每一个值
+	for _, v := range nums {
+		// 累加和一直加即可
+		sum += v
+		// 可以这么理解
+		//  如果存在[0:i] 的和(sum-k), 那么 [i+1:cur] 的和就是k
+		ret += m[sum-k]
+		// 当前位置的个数+1
+		m[sum] += 1
+	}
+	return ret
+}
+
+func main() {
+
+}
