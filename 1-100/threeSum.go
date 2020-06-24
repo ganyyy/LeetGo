a18ef503eb659f7544a4cd6cdc9fd74d401d0fe1
Gann <1354410847@qq.com>
2020-06-24 23:19:55 +0800

    只要思想不滑坡，方法总比困难多

--- 1-100/threeSum.go
+++ 1-100/threeSum.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sort"
 )
 
@@ -98,45 +99,87 @@ func threeSumClosest(nums []int, target int) int {
 	return minVal
 }
 
-func main() {
-	arrNums := [][]int{
-		{1, 1, -1, -1, 3},
-		{1, 2, 4, 8, 16, 32, 64, 128},
-		{1, -3, 3, 5, 4, 1},
-		{0, 1, 2},
-		{0, 2, 1, -3},
-		{1, 1, -1, -1, 3},
-		{1, 1, 1, 0},
-		{13, 2, 0, -14, -20, 19, 8, -5, -13, -3, 20, 15, 20, 5, 13, 14, -17, -7, 12, -6, 0, 20, -19, -1, -15, -2, 8, -2, -9, 13, 0, -3, -18, -9, -9, -19, 17, -14, -19, -4, -16, 2, 0, 9, 5, -7, -4, 20, 18, 9, 0, 12, -1, 10, -17, -11, 16, -13, -14, -3, 0, 2, -18, 2, 8, 20, -15, 3, -13, -12, -2, -19, 11, 11, -10, 1, 1, -10, -2, 12, 0, 17, -19, -7, 8, -19, -17, 5, -5, -10, 8, 0, -12, 4, 19, 2, 0, 12, 14, -9, 15, 7, 0, -16, -5, 16, -12, 0, 2, -16, 14, 18, 12, 13, 5, 0, 5, 6},
-	}
-	arrTarget := []int{
-		-1,
-		82,
-		1,
-		0,
-		1,
-		-1,
-		100,
-		-59,
-	}
-	arrResult := []int{
-		-1,
-		82,
-		1,
-		3,
-		0,
-		-1,
-		3,
-		-58,
-	}
+func threeSumClosest2(nums []int, target int) int {
+	// 先进行排序
+	sort.Ints(nums)
+	res := math.MaxInt32
+	cur := abs(res, target)
+	for i := 0; i < len(nums)-2; i++ {
+		for j, k := i+1, len(nums)-1; j < k; {
+			sum := nums[i] + nums[j] + nums[k]
+			if sum == target {
+				return target
+			}
 
-	for i := 0; i < len(arrResult); i++ {
-		res := threeSumClosest(arrNums[i], arrTarget[i])
-		if res != arrResult[i] {
-			fmt.Printf("i:%d\nnums:%v, \n target:%d, expect:%d, result:%d\n", i, arrNums[i], arrTarget[i], arrResult[i], res)
-			fmt.Printf("-------------------\n")
+			if t := abs(target, sum); cur > t {
+				cur = t
+				res = sum
+			}
+
+			if sum > target {
+				for ; k > j && nums[k] == nums[k-1]; k-- {
+				}
+				k--
+			} else {
+				for ; j < k && nums[j] == nums[j+1]; j++ {
+				}
+				j++
+			}
 		}
 	}
+	return res
+}
+
+func abs(a, b int) int {
+	if a > b {
+		return a - b
+	} else {
+		return b - a
+	}
+}
+
+func main() {
+	//arrNums := [][]int{
+	//	{1, 1, -1, -1, 3},
+	//	{1, 2, 4, 8, 16, 32, 64, 128},
+	//	{1, -3, 3, 5, 4, 1},
+	//	{0, 1, 2},
+	//	{0, 2, 1, -3},
+	//	{1, 1, -1, -1, 3},
+	//	{1, 1, 1, 0},
+	//	{13, 2, 0, -14, -20, 19, 8, -5, -13, -3, 20, 15, 20, 5, 13, 14, -17, -7, 12, -6, 0, 20, -19, -1, -15, -2, 8, -2, -9, 13, 0, -3, -18, -9, -9, -19, 17, -14, -19, -4, -16, 2, 0, 9, 5, -7, -4, 20, 18, 9, 0, 12, -1, 10, -17, -11, 16, -13, -14, -3, 0, 2, -18, 2, 8, 20, -15, 3, -13, -12, -2, -19, 11, 11, -10, 1, 1, -10, -2, 12, 0, 17, -19, -7, 8, -19, -17, 5, -5, -10, 8, 0, -12, 4, 19, 2, 0, 12, 14, -9, 15, 7, 0, -16, -5, 16, -12, 0, 2, -16, 14, 18, 12, 13, 5, 0, 5, 6},
+	//}
+	//arrTarget := []int{
+	//	-1,
+	//	82,
+	//	1,
+	//	0,
+	//	1,
+	//	-1,
+	//	100,
+	//	-59,
+	//}
+	//arrResult := []int{
+	//	-1,
+	//	82,
+	//	1,
+	//	3,
+	//	0,
+	//	-1,
+	//	3,
+	//	-58,
+	//}
+	//
+	//for i := 0; i < len(arrResult); i++ {
+	//	res := threeSumClosest(arrNums[i], arrTarget[i])
+	//	if res != arrResult[i] {
+	//		fmt.Printf("i:%d\nnums:%v, \n target:%d, expect:%d, result:%d\n", i, arrNums[i], arrTarget[i], arrResult[i], res)
+	//		fmt.Printf("-------------------\n")
+	//	}
+	//}
+
+	fmt.Println(threeSumClosest2([]int{-1, 2, 1, -4}, 1))
+
 	/*
 		[]
 		-59
