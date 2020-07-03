805280f3f9f624dd04c333096141b58058cb9c8f
Gann <1354410847@qq.com>
2020-07-04 00:42:37 +0800

    又是DP，还是DP，总是DP. 依旧不会，一直不会，始终不会

--- 1-100/longestValidParentheses.go
+++ 1-100/longestValidParentheses.go
@@ -49,24 +49,29 @@ func longestValidParentheses(s string) int {
 	return res * 2
 }
 
-func dpMatch(s string) int {
+func longestValidParentheses2(s string) int {
 	if len(s) <= 1 {
 		return 0
 	}
+	// dp[i]表示的是  s[i] == ')' 并且 最远的有效匹配 括号长度
 	dp := make([]int, len(s))
 	var res int
 	for i := 1; i < len(s); i++ {
 		// 进行匹配
 		if s[i] == RIGHT {
+			// 前边匹配
 			pre := i - dp[i-1] - 1
 			// 针对 ()
 			if pre >= 0 && s[pre] == LEFT {
-				// 针对 (())
+				// 先加上自己的这两个
+				// 比如 ")(())" 的 dp是[0,0,0,2,4]
 				val := dp[i-1] + 2
-				// 针对 ()()
+				// 针对 ()(), 前边可能有, 也可能没有
 				if pre > 0 {
+					// ")()()"的dp是 [0,0,2,0,4]
 					val += dp[pre-1]
 				}
+				// 更新最大值
 				if val > res {
 					res = val
 				}
@@ -78,5 +83,5 @@ func dpMatch(s string) int {
 }
 
 func main() {
-	fmt.Println(dpMatch("()(()"))
+	fmt.Println(longestValidParentheses2("()(()"))
 }
