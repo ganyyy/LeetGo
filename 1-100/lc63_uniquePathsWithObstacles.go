adc694d8e8043a5bc4284382f9856b62c22a8222
Gann <1354410847@qq.com>
2020-07-06 00:16:30 +0800

    dp 找路径

--- 1-100/lc63_uniquePathsWithObstacles.go
+++ 1-100/lc63_uniquePathsWithObstacles.go
@@ -39,6 +39,49 @@ func uniquePathsWithObstacles(obstacleGrid [][]int) int {
 	return dp[m-1][n-1]
 }
 
+// 代码不一定是越少越好
+func uniquePathsWithObstaclesNew(obstacleGrid [][]int) int {
+	row := len(obstacleGrid)
+	if row == 0 {
+		return 0
+	}
+	col := len(obstacleGrid[0])
+	if col == 0 {
+		return 0
+	}
+	if obstacleGrid[0][0] == 1 || obstacleGrid[row-1][col-1] == 1 {
+		return 0
+	}
+	dp := make([][]int, row)
+	for i := 0; i < row; i++ {
+		dp[i] = make([]int, col)
+	}
+	// 起点
+	dp[0][0] = 1
+	// 第一行, 一路走到尾, 只有一种
+	for i := 1; i < col; i++ {
+		if obstacleGrid[0][i] != 1 {
+			dp[0][i] = dp[0][i-1]
+		}
+	}
+	// 第一列, 一路走到尾, 只有一种
+	for i := 1; i < row; i++ {
+		if obstacleGrid[i][0] != 1 {
+			dp[i][0] = dp[i-1][0]
+		}
+	}
+	// 开始走路
+	for i := 1; i < row; i++ {
+		for j := 1; j < col; j++ {
+			if obstacleGrid[i][j] != 1 {
+				// 左边+上边
+				dp[i][j] = dp[i][j-1] + dp[i-1][j]
+			}
+		}
+	}
+	return dp[row-1][col-1]
+}
+
 func main() {
 
 }
