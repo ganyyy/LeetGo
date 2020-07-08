b3e0a50f5679844d94691b246635d948006a30bc
Gann <1354410847@qq.com>
2020-07-09 01:09:30 +0800

    dp, 字符串分割类型

---
+++ Review/r17_13_replace.go
@@ -0,0 +1,31 @@
+package main
+
+func respace(dictionary []string, sentence string) int {
+	// 到第i个字母时, 最小的改动
+	ln := len(sentence)
+	dp := make([]int, ln+1)
+	// 起始是 0
+	dp[0] = 0
+
+	for i := 1; i <= ln; i++ {
+		// 默认值是都不想等
+		dp[i] = dp[i-1] + 1
+		// 遍历字典, 如果存在相等的子串就进行比较
+		for _, v := range dictionary {
+			if lv := len(v); lv <= i {
+				if v == sentence[i-lv:i] {
+					// 更新最小值
+					dp[i] = min(dp[i], dp[i-lv])
+				}
+			}
+		}
+	}
+	return dp[ln]
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
