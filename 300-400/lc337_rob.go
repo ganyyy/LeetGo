6b4a50696125a80b6855bf69bbaac7de426941e7
Gan <1354410847@qq.com>
2020-08-05 00:22:20 +0800

    树形DP, 依旧是状态转移方程

--- 300-400/lc337_rob.go
+++ 300-400/lc337_rob.go
@@ -3,8 +3,23 @@ package main
 import . "leetgo/data"
 
 func rob(root *TreeNode) int {
-	ret := robInternal(root)
+	ret := robHelper(root)
 	return max(ret[0], ret[1])
+}
+
+// 两个值, 偷当前房子和不偷当前房子
+func robHelper(root *TreeNode) (ret [2]int) {
+	if nil == root {
+		return
+	}
+	// 获取两个孩子的最大价值
+	nextLeft := robHelper(root.Left)
+	nextRight := robHelper(root.Right)
+	// 如果选择取当前节点, 那么两个孩子节点就不能取
+	ret[0] = root.Val + nextLeft[1] + nextRight[1]
+	// 如果不选择当前节点, 就取 两个孩子中的 取和不取 之间的最大值和
+	ret[1] = max(nextLeft[0], nextLeft[1]) + max(nextRight[0], nextRight[1])
+	return
 }
 
 func max(a, b int) int {
@@ -12,22 +27,6 @@ func max(a, b int) int {
 		return a
 	}
 	return b
-}
-
-func robInternal(root *TreeNode) (ret [2]int) {
-	// 返回两个结果:
-	// ret[0] = 不选取本节点
-	// ret[1] = 选取本节点
-	if root == nil {
-		return
-	}
-
-	left := robInternal(root.Left)
-	right := robInternal(root.Right)
-	ret[0] = max(left[0], left[1]) + max(right[0], right[1])
-	ret[1] = left[0] + right[0] + root.Val
-
-	return
 }
 
 func main() {
