7f4599765bba5ad8f3dedc17142ad8d0e3398094
Gan <1354410847@qq.com>
2020-08-07 00:50:10 +0800

    简单的递归

---
+++ 1-100/lc100_isSameTree.go
@@ -0,0 +1,16 @@
+package main
+
+import . "leetgo/data"
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
