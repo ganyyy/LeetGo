2108a64276b7b60b69c4b0ffac08a392836a27fa
Gan <1354410847@qq.com>
2020-08-12 00:29:17 +0800

    简单的DFS

---
+++ 101-200/lc130_cloneGraph.go
@@ -0,0 +1,31 @@
+package main
+
+type Node struct {
+	Val       int
+	Neighbors []*Node
+}
+type M map[int]*Node
+
+func cloneGraph(node *Node) *Node {
+	if nil == node {
+		return nil
+	}
+	m := make(map[int]*Node)
+	return dfs(node, m)
+}
+
+func dfs(n *Node, m M) *Node {
+	if d, ok := m[n.Val]; ok {
+		return d
+	}
+	v := &Node{Val: n.Val}
+	m[v.Val] = v
+	for _, t := range n.Neighbors {
+		if d, ok := m[t.Val]; ok {
+			v.Neighbors = append(v.Neighbors, d)
+		} else {
+			v.Neighbors = append(v.Neighbors, dfs(t, m))
+		}
+	}
+	return v
+}
