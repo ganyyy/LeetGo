1c62ab6987d23b99bb525b45c16d86516f78f121
Gan <1354410847@qq.com>
2020-08-13 01:06:39 +0800

    字符串的乘法

--- 1-100/lc43_multiply.go
+++ 1-100/lc43_multiply.go
@@ -30,6 +30,36 @@ func multiply(num1 string, num2 string) string {
 	}
 	return string(ret)
 }
+func multiply2(num1 string, num2 string) string {
+	res := make([]int, len(num1)+len(num2))
+
+	// 倒序相乘
+	for i := len(num1) - 1; i >= 0; i-- {
+		for j := len(num2) - 1; j >= 0; j-- {
+			m := int(num1[i]-'0') * int(num2[j]-'0')
+			// 加进位
+			m += res[i+j+1]
+			// 高位
+			res[i+j] += m / 10
+			// 低位
+			res[i+j+1] = m % 10
+		}
+	}
+
+	// 去零
+	var i int
+	// 至少保留一位, 允许是 0
+	for ; i < len(res)-1 && res[i] == 0; i++ {
+	}
+
+	// 填充结果
+	r := len(res) - i
+	s := make([]byte, r)
+	for t := 0; t < r; t++ {
+		s[t] = byte(res[t+i] + '0')
+	}
+	return string(s)
+}
 
 func main() {
 	fmt.Println(multiply("123", "456"))
