261b0edacae1055f8dee6e45db3f51c66868bd7b
Gan <1354410847@qq.com>
2020-08-14 00:36:59 +0800

    简单的栈

--- 1-100/isValid.go
+++ 1-100/isValid.go
@@ -22,6 +22,40 @@ func isValid(s string) bool {
 	return len(stack) == 0
 }
 
+// 怎么感觉还退步了?
+func isValid2(s string) bool {
+	stack := make([]byte, 0, len(s))
+	for i := 0; i < len(s); i++ {
+		t := s[i]
+		if t == '(' || t == '{' || t == '[' {
+			stack = append(stack, t)
+		} else {
+			if len(stack) == 0 {
+				return false
+			}
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			switch t {
+			case ')':
+				if top != '(' {
+					return false
+				}
+			case ']':
+				if top != '[' {
+					return false
+				}
+			case '}':
+				if top != '{' {
+					return false
+				}
+			default:
+				return false
+			}
+		}
+	}
+	return len(stack) == 0
+}
+
 func main() {
 	fmt.Println(isValid("([)]"))
 }
