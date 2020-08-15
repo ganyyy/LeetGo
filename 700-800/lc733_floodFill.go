0b9ffbab30a38c680a0e6605fd2992e8972de3d4
Gan <1354410847@qq.com>
2020-08-16 03:04:31 +0800

    dfs

---
+++ 700-800/lc733_floodFill.go
@@ -0,0 +1,38 @@
+package _00_800
+
+func floodFill(image [][]int, sr int, sc int, newColor int) [][]int {
+	old := image[sr][sc]
+
+	// 处理相同的情况
+	if old == newColor {
+		return image
+	}
+	lr := len(image)
+	lc := len(image[0])
+
+	var helper func(int, int)
+
+	helper = func(r, c int) {
+		image[r][c] = newColor
+		v := r - 1
+		if v >= 0 && image[v][c] == old {
+			helper(v, c)
+		}
+		v = r + 1
+		if v < lr && image[v][c] == old {
+			helper(v, c)
+		}
+
+		v = c - 1
+		if v >= 0 && image[r][v] == old {
+			helper(r, v)
+		}
+		v = c + 1
+		if v < lc && image[r][v] == old {
+			helper(r, v)
+		}
+	}
+
+	helper(sr, sc)
+	return image
+}
