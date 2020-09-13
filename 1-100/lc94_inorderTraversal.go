71dff1f623b2161d3b2173f4c5bd88d7ac4cd3cf
Gan <1354410847@qq.com>
2020-09-14 00:41:03 +0800

    重走二叉树

--- 1-100/lc94_inorderTraversal.go
+++ 1-100/lc94_inorderTraversal.go
@@ -6,24 +6,31 @@ import (
 )
 
 func inorderTraversal(root *TreeNode) []int {
-	if root == nil {
-		return []int{}
+	var res []int
+	if nil == root {
+		return res
 	}
-	res := make([]int, 0)
-	stack := make([]*TreeNode, 0)
-	cur := root
-	for cur != nil || len(stack) != 0 {
-		if cur == nil {
-			cur = stack[len(stack)-1]
-			res = append(res, cur.Val)
-			stack = stack[:len(stack)-1]
-			cur = cur.Right
+	var stack []*TreeNode
+	for len(stack) != 0 || root != nil {
+		if root != nil {
+			// 将当前节点入栈
+			stack = append(stack, root)
+			// 指向自己的左孩子
+			root = root.Left
 		} else {
-			stack = append(stack, cur)
-			cur = cur.Left
+			// 栈顶出栈
+			ln := len(stack)
+			last := stack[ln-1]
+			stack = stack[:ln-1]
+			// 将栈顶值输入到输出队列
+			// 可以这么理解: 先找到最后一个左孩子
+			// 如果左孩子没有右子节点, 那么自然会回溯到左孩子的父节点, 满足 左->中->右的输出顺序
+			// 如果左孩子存在右子节点, 那么顺序就是 中->右的输出顺序
+			res = append(res, last.Val)
+			// 指向栈顶元素的右孩子
+			root = last.Right
 		}
 	}
-	//
 	return res
 }
 
