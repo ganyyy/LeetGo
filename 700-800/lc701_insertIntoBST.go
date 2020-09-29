d48023a2aab5c828bf316fb05dd2d6c5981c7aac
Gan <1354410847@qq.com>
2020-09-30 00:35:42 +0800

    二叉搜索树的插入,

---
+++ 700-800/lc701_insertIntoBST.go
@@ -0,0 +1,17 @@
+package _00_800
+
+import (
+	. "leetgo/data"
+)
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+	if root.Val > val {
+		root.Left = insertIntoBST(root.Left, val)
+	} else {
+		root.Right = insertIntoBST(root.Right, val)
+	}
+	return root
+}
