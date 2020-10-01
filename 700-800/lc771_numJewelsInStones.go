ff480faf1a7362d758b2317a7511b273922da712
Gan <1354410847@qq.com>
2020-10-02 02:58:51 +0800

    hello world

---
+++ 700-800/lc771_numJewelsInStones.go
@@ -0,0 +1,27 @@
+package _00_800
+
+func numJewelsInStones(J string, S string) int {
+	src := [52]byte{}
+	for _, v := range J {
+		src[getPos(byte(v))] = 1
+	}
+	var cnt int
+	for i := 0; i < len(S); i++ {
+		if src[getPos(S[i])] != 0 {
+			cnt++
+		}
+	}
+
+	return cnt
+}
+
+func getPos(b byte) byte {
+	if b > 'Z' {
+		return b - 'a' + 26
+	}
+	return b - 'A'
+}
+
+func main() {
+
+}
