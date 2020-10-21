5187661525fa4fd8af38087579e3b99937a109a9
Gan <1354410847@qq.com>
2020-10-22 00:43:05 +0800

    一次AC, 继续加油

---
+++ 700-800/lc763_partitionLabels.go
@@ -0,0 +1,33 @@
+package _00_800
+
+func partitionLabels(S string) []int {
+	// 每个字母在S中占据的最远的位置
+	var dict = [26]int{}
+	var last, pre int
+	for i := 0; i < len(S); i++ {
+		dict[int(S[i]-'a')] = i + 1
+	}
+	// 找到同一区间内最远的位置, 就是分界点
+	var res []int
+	var curLast int
+	for i := 0; i < len(S); i++ {
+		if curLast = dict[S[i]-'a']; curLast != 0 && last != 0 && last == i {
+			res = append(res, last-pre)
+			pre = last
+			last = curLast
+		} else {
+			last = max(last, curLast)
+		}
+	}
+	// 末尾的加上去
+	res = append(res, last-pre)
+
+	return res
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
---
+++ 700-800/lc_test.go
@@ -0,0 +1,8 @@
+package _00_800
+
+import "testing"
+
+func Test_LC763PartitionLabels(t *testing.T) {
+	var ret = partitionLabels("aaaab")
+	t.Log(ret)
+}
