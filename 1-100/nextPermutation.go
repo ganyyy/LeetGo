3d494e13d596a46f15f07f41b172bf191431ff75
Gan <1354410847@qq.com>
2020-11-10 00:52:23 +0800

    二分 还比不过顺序查找? 大吃一惊

--- 1-100/nextPermutation.go
+++ 1-100/nextPermutation.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func nextPermutation(nums []int) {
+func nextPermutation1(nums []int) {
 	if len(nums) < 2 {
 		return
 	}
@@ -37,8 +37,58 @@ func reverse(start int, nums []int) {
 	}
 }
 
+func nextPermutation(nums []int) {
+	if len(nums) <= 1 {
+		return
+	}
+	// 检测是否当前排列是最大的
+	var nextIdx = -1
+	// 从后向前找, 不满足降序的数并记录索引
+	for i := len(nums) - 2; i >= 0; i-- {
+		if nums[i] < nums[i+1] {
+			nextIdx = i
+			break
+		}
+	}
+
+	if nextIdx != -1 {
+		// 找到第一个小于 nums[nextIdx]. 这里使用二分查找
+		var i = nextIdx + findIdx(nums[nextIdx+1:], nums[nextIdx])
+		// 互换最后一个比 nextIdx大的数
+		nums[nextIdx], nums[i] = nums[i], nums[nextIdx]
+		// 后半段逆序处理
+		reverseAll(nums[nextIdx+1:])
+	} else {
+		reverseAll(nums)
+	}
+}
+
+// 整体逆序
+func reverseAll(nums []int) {
+	for l, r := 0, len(nums)-1; l < r; l, r = l+1, r-1 {
+		nums[l], nums[r] = nums[r], nums[l]
+	}
+}
+
+// 二分查找, 后半段是逆序的
+// 我们要找的是 比给定值要大的 最小值.
+// 所以 如果nums中存在和 n相等的值, 需要继续向左边查找
+func findIdx(nums []int, n int) int {
+	var l, r = 0, len(nums)
+	var m int
+	for l < r {
+		m = l + (r-l)>>1
+		if nums[m] <= n {
+			r = m
+		} else {
+			l = m + 1
+		}
+	}
+	return l
+}
+
 func main() {
-	var nums = []int{1, 1, 5}
+	var nums = []int{1, 5, 1}
 	nextPermutation(nums)
 	fmt.Println(nums)
 }
