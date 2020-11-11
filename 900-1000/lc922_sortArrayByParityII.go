36f612934af1ed0bd9ffaec538209de56775e0ec
Gan <1354410847@qq.com>
2020-11-12 00:12:12 +0800

    脑子转不过来了, 不敢想, 也不敢做.

---
+++ 900-1000/lc922_sortArrayByParityII.go
@@ -0,0 +1,22 @@
+package main
+
+func sortArrayByParityII(A []int) []int {
+	l, r := 0, 1
+	for {
+		for l < len(A) && A[l]%2 == 0 {
+			l += 2
+		}
+
+		for r < len(A) && A[r]%2 == 1 {
+			r += 2
+		}
+
+		if l > len(A) || r > len(A) {
+			break
+		}
+
+		A[l], A[r] = A[r], A[l]
+
+	}
+	return A
+}
