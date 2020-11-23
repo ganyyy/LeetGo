4131ce363e7d38c21a2912e54a98b05ff0eea612
Gan <1354410847@qq.com>
2020-11-24 00:23:46 +0800

    二叉树, 叉不起啊

--- 200-300/lc222_countNodes.go
+++ 200-300/lc222_countNodes.go
@@ -3,22 +3,19 @@ package main
 import . "leetgo/data"
 
 func countNodes(root *TreeNode) int {
-	if nil == root {
+	if root == nil {
 		return 0
 	}
-	// 只需要确定子树的高度(左孩子节点的层级)就可以了
-	ld := getDepth(root.Left)
-	rd := getDepth(root.Right)
-	// 左右子树的层级相同, 说明右边最后一层不满, 递归的统计右边的节点个数
-	if ld == rd {
-		// 计算节点个数, ld层一共  2^ld - 1 + 1(根节点数量) = 1 << ld
-		return countNodes(root.Right) + (1 << ld)
+	if ld, rd := getDepth(root.Left), getDepth(root.Right); ld == rd {
+		// 缺口在右半部分, 左边是一个完美二叉树
+		return countNodes(root.Right) + 1<<ld
 	} else {
-		// 左右层级不同, 说明左边最后一层不满, 右边倒数第二层是满的, 递归的统计左子树即可
-		return countNodes(root.Left) + (1 << rd)
+		// 缺口在左半部分, 右半部分是一颗完美二叉树
+		return countNodes(root.Left) + 1<<rd
 	}
 }
 
+// 根据左子树的大小来确定树的高度
 func getDepth(root *TreeNode) int {
 	var res int
 	for root != nil {
@@ -26,6 +23,14 @@ func getDepth(root *TreeNode) int {
 		root = root.Left
 	}
 	return res
+}
+
+func countNodesAll(root *TreeNode) int {
+	// 全遍历一遍是肯定可行的
+	if root == nil {
+		return 0
+	}
+	return countNodes(root.Left) + countNodes(root.Right) + 1
 }
 
 func main() {
