d3a5b03c7cc62563c756b17158c7dcafa8ac363e
Gan <1354410847@qq.com>
2020-12-01 00:25:41 +0800

    二分啊, 我熟

--- 1-100/searchRange.go
+++ 1-100/searchRange.go
@@ -39,6 +39,43 @@ func searchRange(nums []int, target int) []int {
 	return res
 }
 
+func searchRangeNew(nums []int, target int) []int {
+	var left, right = 0, len(nums)
+	if right == 0 {
+		return []int{-1, -1}
+	}
+	// 先看看能不能找得到
+	var res = make([]int, 2)
+	// 左边
+	for left < right {
+		var mid = left + (right-left)/2
+		if nums[mid] >= target {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
+	if left == len(nums) || nums[left] != target {
+		return []int{-1, -1}
+	}
+	res[0] = left
+
+	// 右边
+	left, right = left+1, len(nums)
+	for left < right {
+		var mid = left + (right-left)/2
+		if nums[mid] <= target {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	res[1] = left - 1
+
+	return res
+}
+
 func main() {
 	var nums = []int{7}
 	fmt.Println(searchRange(nums, 8))
