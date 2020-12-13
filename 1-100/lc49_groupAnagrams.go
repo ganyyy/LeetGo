ccaff2d9d7718193ea729a8ca222b113115bf4f6
Gan <1354410847@qq.com>
2020-12-14 00:29:55 +0800

    温故而知新, 可以为师矣

--- 1-100/lc49_groupAnagrams.go
+++ 1-100/lc49_groupAnagrams.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"sort"
+	"unsafe"
+)
+
 func groupAnagrams(strs []string) [][]string {
 	m := make(map[[26]int][]string)
 
@@ -21,6 +26,53 @@ func groupAnagrams(strs []string) [][]string {
 		res = append(res, v)
 	}
 	return res
+}
+
+func groupAnagramsAdvance(strs []string) [][]string {
+	// golang 的字典可以用数组为key
+	var m = make(map[[26]int][]string, len(strs))
+
+	for _, s := range strs {
+		var t [26]int
+		for i := 0; i < len(s); i++ {
+			t[s[i]-'a']++
+		}
+
+		m[t] = append(m[t], s)
+	}
+
+	var res = make([][]string, len(m))
+	var i int
+	for _, ss := range m {
+		res[i] = ss
+		i++
+	}
+
+	return res
+}
+
+func groupAnagramsSort(strs []string) [][]string {
+	mp := map[string][]string{}
+	for _, str := range strs {
+		s := toBytes(str)
+		sort.Slice(s, func(i, j int) bool { return s[i] < s[j] })
+		sortedStr := toString(s)
+		mp[sortedStr] = append(mp[sortedStr], str)
+	}
+	ans := make([][]string, 0, len(mp))
+	for _, v := range mp {
+		ans = append(ans, v)
+	}
+	return ans
+}
+
+func toString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func toBytes(s string) []byte {
+	var x = (*[2]uintptr)(unsafe.Pointer(&s))
+	return *(*[]byte)(unsafe.Pointer(&[3]uintptr{x[0], x[1], x[1]}))
 }
 
 func main() {
