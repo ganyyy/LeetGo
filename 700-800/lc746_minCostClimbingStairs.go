6559bde728e65f1c069e17cfb063203ad948f5cb
Gan <1354410847@qq.com>
2020-12-21 00:23:20 +0800

    活到老, 学到老

---
+++ 700-800/lc746_minCostClimbingStairs.go
@@ -0,0 +1,23 @@
+package _00_800
+
+func minCostClimbingStairs(cost []int) int {
+	// dp. a 表示当前点的前2个位置的消耗, b表示当前点的前1个位置的消耗
+
+	// 初始值是第0个点, 第1个点
+	var a, b = cost[0], cost[1]
+
+	// 一次变换指的是 将a变成b, 将b变成到当前点的最低消耗.
+	// 可由原来的a, b跳到当前点, 所以跳到当前点的最低消耗就是 min(a, b) + cost[i]
+	for i := 2; i < len(cost); i++ {
+		a, b = b, min(a, b)+cost[i]
+	}
+
+	return min(a, b)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
