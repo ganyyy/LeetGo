952fb486e75be929c73f9febdc0398cbaa8444b7
Gan <1354410847@qq.com>
2021-02-04 00:15:09 +0800

    类型转换也会降低程序的执行效率

---
+++ 600-700/lc643_findMaxAverage.go
@@ -0,0 +1,27 @@
+package main
+
+import "math"
+
+func findMaxAverage(nums []int, k int) float64 {
+	var sum float64
+	var kf = float64(k)
+	var res float64 = math.MinInt32
+	for i := 0; i < k; i++ {
+		sum += float64(nums[i])
+	}
+	res = max((sum)/kf, res)
+
+	for i := k; i < len(nums); i++ {
+		sum += float64(nums[i] - nums[i-k])
+		res = max((sum)/(kf), res)
+	}
+
+	return res
+}
+
+func max(a, b float64) float64 {
+	if a > b {
+		return a
+	}
+	return b
+}
