83e52ebb6d39e4ca09ea788ae084f96f32821ba0
Gan <1354410847@qq.com>
2021-02-06 00:57:53 +0800

    有时候, 学会逆向思维很重要. 正着来不行就反着来

---
+++ 1400-1500/lc1423_maxScore.go
@@ -0,0 +1,29 @@
+package main
+
+func maxScore(cardPoints []int, k int) int {
+
+	var ln = len(cardPoints)
+	// 求所有的和, 然后维护一个长度为ln-k的窗口, 保证窗口的和是最小的
+	var sum int
+	for _, v := range cardPoints {
+		sum += v
+	}
+	var cur int
+	for i := 0; i < ln-k; i++ {
+		cur += cardPoints[i]
+	}
+	var mmin = cur
+	for i := ln - k; i < len(cardPoints); i++ {
+		cur += cardPoints[i] - cardPoints[i+k-ln]
+		mmin = min(cur, mmin)
+	}
+	// 最后用总和减去这个值就行
+	return sum - mmin
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
