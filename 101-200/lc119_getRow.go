db1fd9af385ec6e5c92dc2fb1ad87ed064bbd8ac
Gan <1354410847@qq.com>
2021-02-12 00:21:00 +0800

    今年也要继续加油啊

--- 101-200/lc119_getRow.go
+++ 101-200/lc119_getRow.go
@@ -13,3 +13,13 @@ func getRow(rowIndex int) []int {
 
 	return res
 }
+
+func getRowMath(rowIndex int) []int {
+	row := make([]int, rowIndex+1)
+	row[0] = 1
+	// 数学还是好用.
+	for i := 1; i <= rowIndex; i++ {
+		row[i] = row[i-1] * (rowIndex - i + 1) / i
+	}
+	return row
+}
