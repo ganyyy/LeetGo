07f5727ca563de9bb52fb0deb6e1ee973bc4b66f
Gan <1354410847@qq.com>
2021-03-01 00:22:37 +0800

    开始来前缀和了? 送分题罢了

---
+++ 300-400/lc303_NumArray.go
@@ -0,0 +1,29 @@
+package main
+
+type NumArray struct {
+	sum []int
+}
+
+func Constructor(nums []int) NumArray {
+	var tmp = make([]int, len(nums))
+	var cur int
+	for i, v := range nums {
+		cur += v
+		tmp[i] = cur
+	}
+	return NumArray{sum: tmp}
+}
+
+func (n *NumArray) SumRange(i int, j int) int {
+	var sub int
+	if i-1 >= 0 {
+		sub = n.sum[i-1]
+	}
+	return n.sum[j] - sub
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.SumRange(i,j);
+ */
