7e50e58477ed898c354b4c8ac6400259108e4566
Gan <1354410847@qq.com>
2021-03-03 00:34:14 +0800

    啊这, 又是送分题

---
+++ 300-400/lc338_countBits.go
@@ -0,0 +1,30 @@
+package main
+
+func countBits(num int) []int {
+
+	var res = make([]int, num+1)
+
+	// 如何快速的计算bit个数?
+	// 可以通过前缀和实现吗?
+
+	// 如果是奇数, 那么就是前边的偶数的比特位+1
+	// 如果是偶数, 那么就和 >> 1 得到的 奇数/偶数 的比特位相同
+
+	// 初始值肯定是0
+	res[0] = 0
+	for i := 1; i <= num; i++ {
+		if (i & 1) == 1 {
+			// 奇数
+			res[i] = res[i-1] + 1
+		} else {
+			// 偶数
+			res[i] = res[i>>1]
+		}
+
+		// 或许可以再进一步的优化一下
+
+		// res[i] = res[i>>1] + (i & 1)
+	}
+
+	return res
+}
