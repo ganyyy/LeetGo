6c2a18049e7cb1a92386c7a68ffb5d6e1f51eaef
Gan <1354410847@qq.com>
2021-03-15 01:14:35 +0800

    [2] 怎么还不如之前写的?

--- 1-100/lc54_spiralOrder.go
+++ 1-100/lc54_spiralOrder.go
@@ -67,6 +67,112 @@ func spiralOrder(matrix [][]int) []int {
 	return res
 }
 
+func spiralOrder2(matrix [][]int) []int {
+	var n = len(matrix)
+	if n == 0 {
+		return nil
+	}
+	var m = len(matrix[0])
+
+	var res = make([]int, 0, m*n)
+
+	// 可以搞一个递归, 输入边界值决定计算流程
+
+	var helper func(sw, sh, w, h int)
+
+	helper = func(sw, sh, ew, eh int) {
+		// 如果都相等了, 说明还剩一个中间的数, 直接添加并返回即可
+		fmt.Println(sw, sh, ew, eh)
+		if sw > ew || sh > eh {
+			return
+		}
+		// 特殊处理一下两种情况吧
+		// 代码写的太难看了...
+		if sw == ew {
+			for i := sh; i <= eh; i++ {
+				res = append(res, matrix[i][sw])
+			}
+			return
+		}
+		if sh == eh {
+			for i := sw; i <= ew; i++ {
+				res = append(res, matrix[sh][i])
+			}
+			return
+		}
+
+		// 横着
+		for i := sw; i <= ew; i++ {
+			res = append(res, matrix[sh][i])
+		}
+		// 竖着
+		for i := sh + 1; i < eh; i++ {
+			res = append(res, matrix[i][ew])
+		}
+		// 倒横着
+		for i := ew; i >= sw; i-- {
+			res = append(res, matrix[eh][i])
+		}
+		// 倒着竖
+		for i := eh - 1; i > sh; i-- {
+			res = append(res, matrix[i][sw])
+		}
+		helper(sw+1, sh+1, ew-1, eh-1)
+	}
+
+	helper(0, 0, m-1, n-1)
+
+	return res
+}
+
+func spiralOrder3(matrix [][]int) []int {
+	var n = len(matrix)
+	if n == 0 {
+		return nil
+	}
+	var m = len(matrix[0])
+
+	var res = make([]int, 0, m*n)
+
+	var left, right, up, down = 0, m - 1, 0, n - 1
+	for {
+		// 右
+		for i := left; i <= right; i++ {
+			res = append(res, matrix[up][i])
+		}
+		up++ // 右完了是下
+		if up > down {
+			break
+		}
+		// 下
+		for i := up; i <= down; i++ {
+			res = append(res, matrix[i][right])
+		}
+		right-- // 下完了是左
+		if right < left {
+			break
+		}
+		// 左
+		for i := right; i >= left; i-- {
+			res = append(res, matrix[down][i])
+		}
+		down-- // 左完了是上
+		if down < up {
+			break
+		}
+		// 上
+		for i := down; i >= up; i-- {
+			res = append(res, matrix[i][left])
+		}
+		left++ // 上完了是右
+		if left > right {
+			break
+		}
+	}
+
+	return res
+}
+
 func main() {
 	//param := [][]int {
 	//	{1, 2, 3, 4},
