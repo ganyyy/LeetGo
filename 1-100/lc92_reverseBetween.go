7164a3847b0db78ae969a5568a82d12c05fa7ff4
Gan <1354410847@qq.com>
2021-03-18 00:57:00 +0800

    [3] 链表该复习一下了. 都忘的差不多了

--- 1-100/lc92_reverseBetween.go
+++ 1-100/lc92_reverseBetween.go
@@ -36,11 +36,53 @@ func reverseBetween(head *ListNode, m int, n int) *ListNode {
 	}
 }
 
+func reverseBetween2(head *ListNode, left int, right int) *ListNode {
+	var step = 1
+	var pp *ListNode
+	var pre = head
+	var cur = head.Next
+	// 找到left
+	for step < left {
+		pp, pre, cur = pre, cur, cur.Next
+		step++
+	}
+	var split = pre
+	// 翻转到right
+	for step < right {
+		cur.Next, pre, cur = pre, cur, cur.Next
+		step++
+	}
+	// 接尾
+	split.Next = cur
+	if pp != nil {
+		// 接头
+		pp.Next = pre
+		return head
+	}
+	return pre
+}
+
+func reverse2(root *ListNode) *ListNode {
+	if root == nil || root.Next == nil {
+		return root
+	}
+	var pre = root
+	var cur = root.Next
+	// 头节点的下一个节点置为空, 不然就死循环了...
+	pre.Next = nil
+	for cur != nil {
+		var next = cur.Next
+		cur.Next, pre = pre, cur
+		cur = next
+	}
+	return pre
+}
+
 func main() {
 	head := &ListNode{Val: 1}
 	head.Add(2)
 	head.Add(3)
 	head.Add(4)
 	head.Add(5)
-	ShowList(reverseBetween(head, 1, 5))
+	ShowList(reverseBetween2(head, 1, 5))
 }
