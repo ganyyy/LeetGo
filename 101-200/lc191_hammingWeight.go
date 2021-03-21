7959bb189420b669b29a067aed864dd9b0561d6d
Gan <1354410847@qq.com>
2021-03-22 00:28:19 +0800

    [2] 经典回顾, 位运算用的还是不熟练

--- 101-200/lc191_hammingWeight.go
+++ 101-200/lc191_hammingWeight.go
@@ -11,6 +11,15 @@ func hammingWeight(num uint32) int {
 	return count
 }
 
+func hammingWeight2(n uint32) int {
+	n = (n & 0x55555555) + ((n >> 1) & 0x55555555)
+	n = (n & 0x33333333) + ((n >> 2) & 0x33333333)
+	n = (n & 0x0f0f0f0f) + ((n >> 4) & 0x0f0f0f0f)
+	n = (n & 0x00ff00ff) + ((n >> 8) & 0x00ff00ff)
+	n = (n & 0x0000ffff) + ((n >> 16) & 0x0000ffff)
+	return int(n)
+}
+
 func main() {
 
 }
