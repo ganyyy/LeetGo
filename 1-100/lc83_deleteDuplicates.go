5c1b38bb40692aebe23fa359184c9df489aa40ee
Gan <1354410847@qq.com>
2021-03-26 00:15:18 +0800

    [1] 动动脑子还是不错的. 总能想到更好的解法不是嘛

---
+++ 1-100/lc83_deleteDuplicates.go
@@ -0,0 +1,27 @@
+package main
+
+import . "leetgo/data"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	var cur = head
+
+	for cur.Next != nil {
+		if cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	cur.Next = nil
+	return head
+}
