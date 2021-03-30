a19b2ccb0bc48d8b631c4950626caaffc56f628a
Gan <1354410847@qq.com>
2021-03-30 22:58:04 +0800

    [1] 二分, 简单的矩阵查找

--- 1-100/lc74_searchMatrix.go
+++ 1-100/lc74_searchMatrix.go
@@ -27,6 +27,36 @@ func searchMatrix(matrix [][]int, target int) bool {
 	return false
 }
 
+func searchMatrix2(matrix [][]int, target int) bool {
+	// 二分呗.. 这还不好搞..
+	var n = len(matrix)
+	if n == 0 {
+		return false
+	}
+	var m = len(matrix[0])
+	if m == 0 {
+		return false
+	}
+	// 总数
+	n *= m
+	var getVal = func(idx int) int {
+		return matrix[idx/m][idx%m]
+	}
+	var l, r = 0, n
+	var mid int
+	for l < r {
+		mid = l + (r-l)>>1
+		if v := getVal(mid); v == target {
+			return true
+		} else if v > target {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+	return false
+}
+
 func main() {
 	v := [][]int{
 		//{1,   3,  5,  7},
