3d7d05c9a598f0d1783270b027ee51a4b99374b2
Gan <1354410847@qq.com>
2021-04-09 00:17:47 +0800

    [3] 海星. 就这?

--- 101-200/lc154_findMind.go
+++ 101-200/lc154_findMind.go
@@ -1,6 +1,6 @@
 package main
 
-func findMin(nums []int) int {
+func findMin4(nums []int) int {
 	// 因为可能会出现重复字符
 	// 如果出现了, 就意味着中值可能和右边相等
 	// 直接排除即可
@@ -16,6 +16,32 @@ func findMin(nums []int) int {
 		}
 	}
 	return nums[left]
+}
+
+func findMin3(nums []int) int {
+	// 和上道题一样呗, 先去掉重复值在搞
+	var l, r = 0, len(nums) - 1
+	var mid int
+
+	for l < r {
+		for l < r && nums[l] == nums[l+1] {
+			l++
+		}
+		for l < r && nums[r] == nums[r-1] {
+			r--
+		}
+
+		mid = l + (r-l)>>1
+		if nums[mid] > nums[r] {
+			// 左边有序
+			l = mid + 1
+		} else {
+			// 右边有序
+			r = mid
+		}
+	}
+
+	return nums[l]
 }
 
 func main() {
