1f93bb6f2d055edfa47ad54fa380581d24a07400
Gan <1354410847@qq.com>
2021-04-13 00:25:53 +0800

    [2] 遇到二叉搜索树, 直接中序就完事了. 区别是使用递归还是循环

---
+++ 700-800/lc783_minDiffInBST.go
@@ -0,0 +1,66 @@
+package main
+
+import "math"
+import . "leetgo/data"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDiffInBST(root *TreeNode) int {
+	var res = math.MaxInt32
+	var pre = math.MaxInt32
+
+	var dfs func(root *TreeNode)
+	dfs = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		dfs(root.Left)
+		if pre != math.MaxInt32 {
+			if t := root.Val - pre; t < res {
+				res = t
+			}
+		}
+		pre = root.Val
+		dfs(root.Right)
+	}
+	dfs(root)
+
+	return res
+}
+
+// 循环版
+func minDiffInBSTLoop(root *TreeNode) int {
+	var res = math.MaxInt32
+	var pre = math.MaxInt32
+
+	var stack []*TreeNode
+
+	for root != nil || len(stack) != 0 {
+		if root == nil {
+			root = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			if pre != math.MaxInt32 {
+				res = min(res, root.Val-pre)
+			}
+			pre = root.Val
+			root = root.Right
+		} else {
+			stack = append(stack, root)
+			root = root.Left
+		}
+	}
+	return res
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
