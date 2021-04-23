01673e1612debd3d85538fef9e7d38e9718ccb80
Gan <1354410847@qq.com>
2021-04-24 00:31:29 +0800

    [2] DP啊, 灵光一闪很重要

---
+++ 300-400/lc377_combinationSum4.go
@@ -0,0 +1,30 @@
+package main
+
+import "sort"
+
+func combinationSum4(nums []int, target int) int {
+	// dfs 搞一下. 十有八九会溢出吧
+
+	// 不进行sort也没问题. 但是sort之后可以方便的进行提前退出循环.
+	sort.Ints(nums)
+
+	// 如果出现了负数, 那么依旧可以通过统一加入最小值的绝对值将其转换为整数数组进行处理
+	// 需要注意的点为: 相加不能超过 int的最大表示范围
+
+	// dp 表示 到达每一个数字的可能次数
+	var dp = make([]int, target+1)
+	dp[0] = 1
+	for i := 1; i <= target; i++ {
+		for j := 0; j < len(nums); j++ {
+			if i == nums[j] {
+				dp[i] += 1
+				break
+			} else if i > nums[j] {
+				dp[i] += dp[i-nums[j]]
+			} else {
+				break
+			}
+		}
+	}
+	return dp[target]
+}
