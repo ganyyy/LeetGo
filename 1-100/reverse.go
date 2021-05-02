72b09dd541b491da4e39e42b3d0beded05f86192
Gan <1354410847@qq.com>
2021-05-03 01:10:38 +0800

    [1] 考虑加个Rust仓库?

--- 1-100/reverse.go
+++ 1-100/reverse.go
@@ -6,30 +6,20 @@ import (
 )
 
 func reverse(x int) int {
-	end := 0
-	if x < 0 {
-		end = 1
-		x = -x
-	}
 	res := 0
 
-	for x > 0 {
+	for x != 0 {
 		res = res*10 + x%10
 		x = x / 10
 	}
 
-	if end == 1 {
-		if -res < math.MinInt32 {
-			return 0
-		}
-		return -res
-	}
-	if res > math.MaxInt32 {
+	if res > math.MaxInt32 || res < math.MinInt32 {
 		return 0
 	}
 	return res
 }
 
 func main() {
+	fmt.Println(-12 % 10)
 	fmt.Println(reverse(-123))
 }
