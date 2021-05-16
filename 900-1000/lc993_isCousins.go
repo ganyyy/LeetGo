508b4415ad6f13bea2958b8fb7a623d662b7dea3
Gan <1354410847@qq.com>
2021-05-17 00:25:40 +0800

    [1] 层次遍历

---
+++ 900-1000/lc993_isCousins.go
@@ -0,0 +1,57 @@
+package main
+
+import . "leetgo/data"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type myNode struct {
+	*TreeNode
+	parent *TreeNode
+}
+
+func isCousins(r *TreeNode, x int, y int) bool {
+	// 层次遍历
+	var stack = []*myNode{&myNode{r, nil}}
+	var fa, fb *myNode
+	var tmp []*myNode
+	var root *myNode
+	for len(stack) != 0 {
+		fa, fb = nil, nil
+		for i := 0; i < len(stack); i++ {
+			root = stack[i]
+			if root.Val == x {
+				fa = root
+			} else if root.Val == y {
+				fb = root
+			}
+			if fa != nil && fb != nil {
+				if fa.parent == fb.parent {
+					return false
+				}
+				return true
+			}
+			if root.Left != nil {
+				tmp = append(tmp, &myNode{root.Left, root.TreeNode})
+			}
+			if root.Right != nil {
+				tmp = append(tmp, &myNode{root.Right, root.TreeNode})
+			}
+		}
+		// 找到任意一个均可以提前返回
+		if fa != nil || fb != nil {
+			return false
+		}
+		stack = make([]*myNode, len(tmp))
+		copy(stack, tmp)
+		tmp = tmp[:0]
+	}
+
+	return false
+}
