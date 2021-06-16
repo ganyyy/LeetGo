b0bca27e07c9b7b5737ef7af413209161cdfa96d
Gan <1354410847@qq.com>
2021-06-17 00:39:56 +0800

    [4] 状态机, 还是要看看的

--- 1-100/lc65_isNumber.go
+++ 1-100/lc65_isNumber.go
@@ -53,6 +53,58 @@ func isNumber(s string) bool {
 	return true
 }
 
+func isNumber2(s string) bool {
+	s = strings.TrimSpace(s)
+	if len(s) == 0 {
+		return false
+	}
+	isE := false     // 指数
+	isSmall := false // 小数
+	isFlag := false  // 负数
+	isDig := false   // 整数
+	// e 后面可以跟负数, 不能跟小数
+	// -后边不能跟-
+	// .后边不能跟
+	for i := 0; i < len(s); i++ {
+		switch s[i] {
+		case 'e', 'E':
+			// e前边需要有数字, e后边不能跟小数
+			if !isDig || isE {
+				return false
+			}
+			isE = true
+			isSmall = true
+			// e后边可以有符号和数字
+			isFlag = false
+			isDig = false
+		case '.':
+			// 小数点只能在e前边, 并且只能出现一次
+			if isSmall || isE {
+				return false
+			}
+			isSmall = true
+		case '+', '-':
+			// 前边是数字或者前边是负数或者
+			// 不是e开头的小数
+			if isDig || (isSmall && !isE) || isFlag {
+				return false
+			}
+			isFlag = true
+		case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0':
+			isDig = true
+			// 只要出现一个数字, 就不能是负数
+			isFlag = true
+		default:
+			// 不在合法范围内的字符
+			return false
+		}
+	}
+	if (isE || isSmall || isFlag) && !isDig {
+		return false
+	}
+	return true
+}
+
 func main() {
 	ss := []string{
 		//"0",
