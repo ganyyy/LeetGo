e7a8d56d8812fd938139eff4e54e2bc50bd6bac0
Gan <1354410847@qq.com>
2021-07-02 00:59:53 +0800

    [2] 无限背包

---
+++ 300-400/lc322_coinChange.go
@@ -0,0 +1,36 @@
+package main
+
+import "math"
+
+func coinChange(coins []int, amount int) int {
+	// 无限背包, 每一个可以重复选择
+
+	var dp = make([]int, amount+1)
+	for i := range dp {
+		dp[i] = math.MaxInt32
+	}
+
+	dp[0] = 0
+
+	for i := 1; i <= amount; i++ {
+		for j := 0; j < len(coins); j++ {
+			if coins[j] <= i {
+				dp[i] = min(dp[i], dp[i-coins[j]]+1)
+			}
+		}
+	}
+
+	// fmt.Println(dp)
+	if dp[amount] == math.MaxInt32 {
+		return -1
+	}
+
+	return dp[amount]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
