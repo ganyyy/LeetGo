47f644b4c54fa0d990419c60d1e822a2a695ae31
Gan <1354410847@qq.com>
2021-07-13 08:04:28 +0800

    [5] 虽然看不懂, 但是我大为震撼

---
+++ 200-300/lc218_getSkyline.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"container/heap"
+	"sort"
+)
+
+type pair struct{ right, height int }
+type hp []pair
+
+func (h hp) Len() int            { return len(h) }
+func (h hp) Less(i, j int) bool  { return h[i].height > h[j].height }
+func (h hp) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *hp) Push(v interface{}) { *h = append(*h, v.(pair)) }
+func (h *hp) Pop() interface{}   { a := *h; v := a[len(a)-1]; *h = a[:len(a)-1]; return v }
+
+func getSkyline(buildings [][]int) (ans [][]int) {
+	n := len(buildings)
+	boundaries := make([]int, 0, n*2)
+	for _, building := range buildings {
+		// 线段的起点和终点. 关键点总是落在左右边缘上.
+		boundaries = append(boundaries, building[0], building[1])
+	}
+	sort.Ints(boundaries)
+
+	idx := 0
+	h := hp{}
+	for _, boundary := range boundaries {
+		// 找到所有和当前建筑有交集的楼
+		for idx < n && buildings[idx][0] <= boundary {
+			heap.Push(&h, pair{buildings[idx][1], buildings[idx][2]})
+			idx++
+		}
+		//
+		for len(h) > 0 && h[0].right <= boundary {
+			heap.Pop(&h)
+		}
+
+		maxn := 0
+		if len(h) > 0 {
+			maxn = h[0].height
+		}
+		if len(ans) == 0 || maxn != ans[len(ans)-1][1] {
+			ans = append(ans, []int{boundary, maxn})
+		}
+	}
+	return
+}
