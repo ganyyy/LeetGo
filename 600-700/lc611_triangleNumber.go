e9f27108909d81966d301a16e4637d8e37ba9fd2
Gan <1354410847@qq.com>
2021-08-04 00:54:16 +0800

    [4] 合理利用双指针

---
+++ 600-700/lc611_triangleNumber.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func triangleNumberBad(nums []int) int {
+	var cnt int
+	sort.Ints(nums)
+
+	// 这太暴力了吧
+	for i := 0; i < len(nums)-2; i++ {
+		for j := i + 1; j < len(nums)-1; j++ {
+			// 这一步可以优化成二分查找, 找到距离max最短的位置
+			var max = nums[i] + nums[j]
+			for k := j + 1; k < len(nums); k++ {
+				if max > nums[k] {
+					cnt++
+				} else {
+					break
+				}
+			}
+		}
+	}
+	return cnt
+}
+
+func triangleNumber(nums []int) int {
+	var cnt int
+	sort.Ints(nums)
+
+	for i := len(nums) - 1; i > 1; i-- {
+		// 双指针的正确用法
+		var l, r = 0, i - 1
+		for l < r {
+			if nums[l]+nums[r] > nums[i] {
+				cnt += r - l
+				r--
+			} else {
+				l++
+			}
+		}
+	}
+	return cnt
+}
+
+func main() {
+	var src = []int{1, 2, 3, 3, 4, 5, 6}
+	var idx = sort.Search(len(src), func(i int) bool {
+		return src[i] > 3
+	})
+	fmt.Println(idx)
+}
