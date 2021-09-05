f21414084f717c0388ddd86ca9b21cb57d89b3a6
Gan <1354410847@qq.com>
2021-09-05 21:24:42 +0800

    [4] 概率拟合

---
+++ 400-500/lc470_rand10.go
@@ -0,0 +1,19 @@
+package main
+
+func rand7() int {
+	return 0
+}
+
+func rand10() int {
+	// 简单而言, 随机概率
+
+	// 构造出一片随机集合, 保留其中等概率出现的一部分
+	// 然后对其进行取余
+
+	// 这里获取的是 [1,7] + [1,7] 的所有的结果, 拒绝 一部分数据保证剩余数字满足等概率出现
+	var a, b = rand7(), rand7()
+	if a > 4 && b < 4 {
+		return rand10()
+	}
+	return (a+b)%10 + 1
+}
