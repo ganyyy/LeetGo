99ebf068bf0fa8b3b68a8d0132eb25a9f410ac8f
Gan <1354410847@qq.com>
2021-09-28 08:00:52 +0800

    [5] 区间和

---
+++ 400-500/lc437_pathSum.go
@@ -0,0 +1,35 @@
+package main
+
+import . "leetgo/data"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func pathSum(root *TreeNode, targetSum int) int {
+
+	var count = make(map[int]int)
+	count[0] = 1
+
+	var helper func(root *TreeNode, pre int) int
+	helper = func(root *TreeNode, pre int) int {
+		if root == nil {
+			return 0
+		}
+		var res int
+		pre += root.Val
+		// 还是区间和. 本质上就是 sum[:i] - sum[:j] == target, 即为 sum[i:j] = target
+		res += count[pre-targetSum]
+		count[pre]++
+		res += helper(root.Left, pre) + helper(root.Right, pre)
+		count[pre]--
+
+		return res
+	}
+
+	return helper(root, 0)
+}
