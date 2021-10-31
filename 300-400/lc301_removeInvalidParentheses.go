626715e9fc7d3a6456225eddfbc8612aca8c2ad2
Gan <1354410847@qq.com>
2021-10-31 19:13:24 +0800

    [每周回顾] 合理的枝减很重要

--- 300-400/lc301_removeInvalidParentheses.go
+++ 300-400/lc301_removeInvalidParentheses.go
@@ -27,8 +27,9 @@ func removeInvalidParentheses(s string) []string {
 	var isValid = func(a, b int) bool {
 		return a == 0 && b == 0
 	}
-	var rl, rr = check([]byte(s))
-	var left, right = rl, rr
+
+	// 计算不合法的左括号/右括号数量, 这个就是要删除的括号
+	var left, right = check([]byte(s))
 
 	var set = make(map[string]bool)
 
@@ -70,4 +71,64 @@ func removeInvalidParentheses(s string) []string {
 	helper(0)
 
 	return ret
+}
+
+func isValid(str string) bool {
+	cnt := 0
+	for _, ch := range str {
+		if ch == '(' {
+			cnt++
+		} else if ch == ')' {
+			cnt--
+			if cnt < 0 {
+				return false
+			}
+		}
+	}
+	return cnt == 0
+}
+
+func helper(ans *[]string, str string, start, lRemove, rRemove int) {
+	if lRemove == 0 && rRemove == 0 {
+		if isValid(str) {
+			*ans = append(*ans, str)
+		}
+		return
+	}
+
+	for i := start; i < len(str); i++ {
+		if i != start && str[i] == str[i-1] {
+			continue
+		}
+		// 如果剩余的字符无法满足去掉的数量要求，直接返回
+		if lRemove+rRemove > len(str)-i {
+			return
+		}
+		// 尝试去掉一个左括号
+		if lRemove > 0 && str[i] == '(' {
+			helper(ans, str[:i]+str[i+1:], i, lRemove-1, rRemove)
+		}
+		// 尝试去掉一个右括号
+		if rRemove > 0 && str[i] == ')' {
+			helper(ans, str[:i]+str[i+1:], i, lRemove, rRemove-1)
+		}
+	}
+}
+
+func removeInvalidParenthesesFast(s string) (ans []string) {
+	lRemove, rRemove := 0, 0
+	for _, ch := range s {
+		if ch == '(' {
+			lRemove++
+		} else if ch == ')' {
+			if lRemove == 0 {
+				rRemove++
+			} else {
+				lRemove--
+			}
+		}
+	}
+
+	helper(&ans, s, 0, lRemove, rRemove)
+	return
 }
