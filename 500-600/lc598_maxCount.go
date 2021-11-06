f7e202e6f1f09c603667b5c3eb70ae45e74cd11b
Gan <1354410847@qq.com>
2021-11-07 00:23:14 +0800

    [0] 懒得看

---
+++ 500-600/lc598_maxCount.go
@@ -0,0 +1,20 @@
+package main
+
+func maxCount(m int, n int, ops [][]int) int {
+	// 简单而言, 就是求交集罢了..
+
+	var x, y = m, n
+
+	for _, op := range ops {
+		x, y = min(op[0], x), min(op[1], y)
+	}
+
+	return x * y
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
