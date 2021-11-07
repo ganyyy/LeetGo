9042208893b1b2f5515bbd71a8316b3cc99e7537
Gan <1354410847@qq.com>
2021-11-07 20:53:11 +0800

    [每周回顾] 堆?  双指针?

--- 1-100/firstMissingPositive.go
+++ 1-100/firstMissingPositive.go
@@ -5,6 +5,9 @@ import "fmt"
 func firstMissingPositive(nums []int) int {
 	count := len(nums)
 	for i := 0; i < count; i++ {
+		// 定位重置, 给每个正整数找到其应有的位置
+		// 这里有两点需要注意一下:
+		// 负数不需要处理, 超过了数组上限的数不需要处理
 		for v := nums[i]; v <= count && v > 0 && v != nums[v-1]; v = nums[i] {
 			nums[i], nums[v-1] = nums[v-1], nums[i]
 		}
@@ -18,5 +21,5 @@ func firstMissingPositive(nums []int) int {
 }
 
 func main() {
-	fmt.Println(firstMissingPositive([]int{7, 8, 9, 11, 12}))
+	fmt.Println(firstMissingPositive([]int{1, 7, 8, 9, 11, 12}))
 }
--- 400-500/lc407_trapRainWater.go
+++ 400-500/lc407_trapRainWater.go
@@ -23,8 +23,11 @@ func trapRainWater(heightMap [][]int) (ans int) {
 		visit[i] = make([]bool, n)
 	}
 
+	// 本质上和二维的没啥区别, 二维的是从两头向中间缩
+	// 三维的是从四周向中间缩
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
+			// 将所有外围边入队.
 			if i == 0 || i == m-1 || j == 0 || j == n-1 {
 				p.add(&pair{x: i, y: j, h: heightMap[i][j]})
 				visit[i][j] = true
@@ -36,14 +39,19 @@ func trapRainWater(heightMap [][]int) (ans int) {
 	dc := []int{1, 0, -1, 0}
 
 	for !p.isEmpty() {
+		// 每次都取最低点是为了保证其后续的节点一定可以存水
 		cur := p.poll()
 		x, y, h := cur.x, cur.y, cur.h
 		for k := 0; k < 4; k++ {
+			// 对比四个方向
 			newx, newy := x+dr[k], y+dc[k]
 			if 0 <= newx && newx < m && 0 <= newy && newy < n && !visit[newx][newy] {
+				// 如果获取到的新的位置的高度低于当前值(当前值已经是队列中的最低点了)
+				// 那么该位置就可以存水
 				if heightMap[newx][newy] < h {
 					ans += h - heightMap[newx][newy]
 				}
+				// 将点位入队
 				p.add(&pair{x: newx, y: newy, h: max(h, heightMap[newx][newy])})
 				visit[newx][newy] = true
 			}
@@ -77,6 +85,7 @@ func (p *PriQueue) add(data *pair) {
 		return
 	}
 	p.count++
+	// 每次都将节点放置到最后, 然后向前卷
 	p.arr[p.count] = data
 	i := p.count
 	for i/2 > 0 && p.arr[i/2].h > p.arr[i].h {
@@ -86,6 +95,7 @@ func (p *PriQueue) add(data *pair) {
 }
 
 func (p *PriQueue) poll() *pair {
+	// 获取堆顶的最小元素
 	res := p.arr[1]
 	p.arr[1] = p.arr[p.count]
 	p.count--
@@ -94,8 +104,10 @@ func (p *PriQueue) poll() *pair {
 }
 
 func heapify(arr []*pair, size int, i int) {
+	// 将指定位置的元素放置到合适的位置上
 	for {
 		pos := i
+		// 这是一个小顶堆
 		if i*2 <= size && arr[i*2].h < arr[i].h {
 			pos = i * 2
 		}
