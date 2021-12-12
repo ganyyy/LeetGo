71c99f81aba5095f0d1628b39ffe7d639e60d3e1
Gan <1354410847@qq.com>
2021-12-13 00:24:58 +0800

    [0] 看懂不难, 难的是阅读理解. leet code 的翻译早该突突了

---
+++ 800-900/lc807_maxIncreaseKeepingSkyline.go
@@ -0,0 +1,36 @@
+package main
+
+func maxIncreaseKeepingSkyline(grid [][]int) int {
+	// 统计每行/每列的最大值
+
+	var maxRow, maxCol = make([]int, len(grid)), make([]int, len(grid[0]))
+
+	for i := 0; i < len(grid); i++ {
+		for j, v := range grid[i] {
+			maxRow[i] = max(maxRow[i], v)
+			maxCol[j] = max(maxCol[j], v)
+		}
+	}
+	var ret int
+	for i := 0; i < len(grid); i++ {
+		for j, v := range grid[i] {
+			ret += min(maxRow[i], maxCol[j]) - v
+		}
+	}
+
+	return ret
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
