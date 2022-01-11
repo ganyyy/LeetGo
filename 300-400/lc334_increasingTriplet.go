d907dd3c7499e81932ea74f16b517e9a286dd69b
Gan <1354410847@qq.com>
2022-01-12 00:21:01 +0800

    [2] 当成LIS来搞了

---
+++ 300-400/lc334_increasingTriplet.go
@@ -0,0 +1,46 @@
+package main
+
+import "math"
+
+func increasingTriplet(nums []int) bool {
+	// 三个数,
+
+	var src = make([]int, 0, 3)
+next:
+	for _, v := range nums {
+		if len(src) == 0 {
+			src = append(src, v)
+			continue
+		}
+		for j, v2 := range src {
+			if v <= v2 {
+				src[j] = v
+				continue next
+			}
+		}
+		src = append(src, v)
+		if len(src) == 3 {
+			return true
+		}
+	}
+	return false
+}
+
+func increasingTriplet2(nums []int) bool {
+	n := len(nums)
+	if n < 3 {
+		return false
+	}
+	var a, b = math.MaxInt32, math.MaxInt32
+
+	for _, v := range nums {
+		if a >= v {
+			a = v
+		} else if b >= v {
+			b = v
+		} else {
+			return true
+		}
+	}
+	return false
+}
