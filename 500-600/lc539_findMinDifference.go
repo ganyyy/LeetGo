ad181a65014179cac42b09b35678a5c9588364c0
Gan <1354410847@qq.com>
2022-01-18 00:29:20 +0800

    [1] easy!

---
+++ 500-600/lc539_findMinDifference.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"sort"
+	"strconv"
+)
+
+func findMinDifference(timePoints []string) int {
+	const DAY = 24 * 60
+
+	// 一天就1440分钟, 超过这个时间点肯定有重复的
+	if len(timePoints) > DAY {
+		return 0
+	}
+	sort.Strings(timePoints)
+
+	var parse = func(str string) int {
+		var hour, minute = str[:2], str[3:]
+		var h, _ = strconv.Atoi(hour)
+		var m, _ = strconv.Atoi(minute)
+		return h*60 + m
+	}
+
+	var a, b = parse(timePoints[0]), parse(timePoints[1])
+	var ret = b - a
+
+	for i := 2; i < len(timePoints); i++ {
+		a, b = b, parse(timePoints[i])
+		ret = min(ret, b-a)
+	}
+
+	ret = min(ret, DAY+parse(timePoints[0])-b)
+	return ret
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
