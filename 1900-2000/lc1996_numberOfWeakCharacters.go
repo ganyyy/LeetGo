0391689fe767b67fd8ffa3771724e09b8de4e33d
Gan <1354410847@qq.com>
2022-01-28 00:33:23 +0800

    [3] 我是SB

---
+++ 1900-2000/lc1996_numberOfWeakCharacters.go
@@ -0,0 +1,80 @@
+package main
+
+import "sort"
+
+func numberOfWeakCharactersSB(properties [][]int) int {
+	// 分优先级进行排序
+
+	sort.Slice(properties, func(i, j int) bool {
+		var a, b = properties[i], properties[j]
+		if a[0] != b[0] {
+			return a[0] < b[0]
+		}
+		return a[1] < b[1]
+	})
+
+	// 合并相同的分组(?)
+
+	// 现在可以保证的是: 前者的攻击力一定小于后者
+	// 攻击力相同时, 前者的防御一定小于后者
+
+	// 用单调递增栈(栈顶最小), 如果一个数成为新的栈顶, 就说明不存在比他更小的值
+
+	var stack []int
+	var ret int
+	for i, p := range properties {
+		if len(stack) == 0 {
+			stack = append(stack, i)
+			continue
+		}
+		for len(stack) > 0 {
+			var top = properties[stack[len(stack)-1]]
+			if top[0] < p[0] && top[1] < p[1] {
+				ret++
+				stack = stack[:len(stack)-1]
+			} else {
+				// 从头扫一遍?
+				for i := 0; i < len(stack); {
+					var cur = properties[stack[i]]
+					if cur[0] < p[0] && cur[1] < p[1] {
+						copy(stack[i:], stack[i+1:])
+						stack = stack[:len(stack)-1]
+						ret++
+					} else {
+						i++
+					}
+				}
+				break
+			}
+		}
+		stack = append(stack, i)
+	}
+
+	return ret
+}
+
+func numberOfWeakCharacters(properties [][]int) int {
+	// 攻击降序, 防御升序. 避免了同攻击时误判的情况
+	sort.Slice(properties, func(i, j int) bool {
+		var a, b = properties[i], properties[j]
+		if a[0] != b[0] {
+			return a[0] > b[0]
+		}
+		return a[1] < b[1]
+	})
+
+	// fmt.Println(properties)
+
+	var top = properties[0]
+	var maxDef = top[1]
+	var cnt int
+	for _, p := range properties[1:] {
+		if p[1] < maxDef {
+			cnt++
+		} else {
+			maxDef = p[1]
+		}
+	}
+
+	return cnt
+}
