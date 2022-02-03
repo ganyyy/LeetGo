7cdbbcfc71870f228c6b07a4cfd4df7ebbe63e84
Gan <1354410847@qq.com>
2022-02-04 00:20:02 +0800

    [1] 简单题啊, 搞不明白了?

---
+++ 1700-1800/lc1725_countGoodRectangles.go
@@ -0,0 +1,31 @@
+package main
+
+func countGoodRectangles(rectangles [][]int) int {
+	var cnt int
+	var m int
+	for _, r := range rectangles {
+		var side = minSide(r)
+		if side == m {
+			cnt++
+		} else if side > m {
+			m = side
+			cnt = 1
+		}
+	}
+	return cnt
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func minSide(r []int) int {
+	var a, b = r[0], r[1]
+	if a < b {
+		return a
+	}
+	return b
+}
