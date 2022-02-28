fc3a8281ccf6699551c8519d79f65766a399e625
Gan <1354410847@qq.com>
2022-03-01 00:35:21 +0800

    [1] 三年进步了个啥?

--- 1-100/convert.go
+++ 1-100/convert.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 func convert(s string, numRows int) string {
 	if numRows == 1 {
-		 return s
+		return s
 	}
 	size := len(s)
 	res := make([]byte, 0, size)
@@ -15,17 +18,41 @@ func convert(s string, numRows int) string {
 		for j < size {
 			res = append(res, s[j])
 			if i != 0 && i != numRows-1 {
-			// 不是开头和结尾的情况, 求定点的和在减去当前值即为中间值
-				val := sub*(2 * mul - 1) - j
+				// 不是开头和结尾的情况, 求定点的和在减去当前值即为中间值
+				val := sub*(2*mul-1) - j
 				if val < size {
 					res = append(res, s[val])
 				}
 			}
 			j += sub
-			mul +=1
+			mul += 1
 		}
 	}
 	return string(res)
+}
+
+func convert2(s string, numRows int) string {
+	if numRows == 1 {
+		return s
+	}
+	var next = [2]int{0, (numRows - 1) * 2}
+	var ret = bytes.NewBuffer(nil)
+	ret.Grow(len(s))
+	for i := 0; i < numRows; i++ {
+		var cur = i
+		var cnt int
+		for cur < len(s) {
+			ret.WriteByte(s[cur])
+			cnt++
+			if n := next[cnt&1]; n == 0 {
+				cnt++
+			}
+			cur += next[cnt&1]
+		}
+		next[0], next[1] = next[0]+2, next[1]-2
+	}
+
+	return ret.String()
 }
 
 func main() {
