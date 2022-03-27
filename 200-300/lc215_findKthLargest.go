1ef9c293a879b5724553416eb5d70e387c3af6b5
Gan <1354410847@qq.com>
2022-03-27 17:33:12 +0800

    [老题回顾] 随便看看

--- 200-300/lc215_findKthLargest.go
+++ 200-300/lc215_findKthLargest.go
@@ -43,6 +43,44 @@ func findKthLargest(nums []int, k int) int {
 	return t[0]
 }
 
+func findKthLargest2(nums []int, k int) int {
+	var arr = make([]int, k)
+	var heapArr = func() {
+		// 这是一个小顶堆, 那么堆顶一定是最小值
+		for i := k/2 - 1; i >= 0; i-- {
+			var root = i
+			for {
+				var child = root*2 + 1
+				if child >= k {
+					break
+				}
+				if child+1 < k && arr[child+1] < arr[child] {
+					child++
+				}
+				if arr[child] >= arr[root] {
+					break
+				}
+				// 这一步是关键欸
+				arr[root], arr[child] = arr[child], arr[root]
+				root = child
+			}
+		}
+	}
+
+	copy(arr, nums[:k])
+	heapArr()
+
+	for _, v := range nums[k:] {
+		if v <= arr[0] {
+			continue
+		}
+		arr[0], arr[k-1] = arr[k-1], v
+		heapArr()
+	}
+
+	return arr[0]
+}
+
 func main() {
 	fmt.Println(findKthLargest([]int{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4))
 }
