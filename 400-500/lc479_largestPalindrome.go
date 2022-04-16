aed23934cfaaa24760ab5f733a7629ff792b46a1
Gan <1354410847@qq.com>
2022-04-16 18:12:01 +0800

    [5] 不如打表

---
+++ 400-500/lc479_largestPalindrome.go
@@ -0,0 +1,23 @@
+package main
+
+import "math"
+
+func largestPalindrome(n int) int {
+	if n == 1 {
+		return 9
+	}
+	// n位数的上限
+	upper := int(math.Pow10(n)) - 1
+	for left := upper; ; left-- { // 枚举回文数的左半部分
+		p := left
+		for x := left; x > 0; x /= 10 {
+			p = p*10 + x%10 // 翻转左半部分到其自身末尾，构造回文数 p
+		}
+		// x属于较大的值
+		for x := upper; x*x >= p; x-- {
+			if p%x == 0 { // x 是 p 的因子
+				return p % 1337
+			}
+		}
+	}
+}
