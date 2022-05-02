9922bb1ebdb13d257dc1860ae4ddd38f6fcc5872
Gan <1354410847@qq.com>
2022-05-02 22:19:00 +0800

    [4] 值得一看的DFS+BT

--- 101-200/lc124_maxPathSum.go
+++ 101-200/lc124_maxPathSum.go
@@ -1,3 +1,5 @@
+//go:build ignore
+
 package main
 
 import (
@@ -5,21 +7,17 @@ import (
 	"math"
 )
 
-import . "leetgo/data"
-
 /*
 给定一个非空二叉树，返回其最大路径和。
 本题中，路径被定义为一条从树中任意节点出发，达到任意节点的序列。该路径至少包含一个节点，且不一定经过根节点。
 */
 
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
 func maxPathSum(root *TreeNode) int {
 	var m = math.MinInt32
 	r(root, &m)
@@ -45,6 +43,37 @@ func max(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func maxPathSumNew(root *TreeNode) int {
+	var max = func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+	var m = math.MinInt32
+
+	var dfs func(root *TreeNode) int // 以root为根节点, 最大的路径和
+
+	dfs = func(root *TreeNode) int {
+		if root == nil {
+			return math.MinInt32 // 不能返回0, 因为可能会出现比0小的值
+		}
+		var left, right = dfs(root.Left), dfs(root.Right)
+		var val = root.Val
+		var maxSub = max(left, right)
+		var cur = max(val, val+maxSub) // 当前根节点的最大路径和
+
+		// 可以是之前的最大值, 也可以是当前值, 也可以是左右子树的最大值, 也可以是根+左+右的值
+		m = max(m, max(cur, max(maxSub, val+left+right))) // 累积的最大路径和
+
+		return cur
+	}
+
+	dfs(root)
+
+	return m
 }
 
 func main() {
