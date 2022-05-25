fc48a01e4799882da02e3bacbe001f81a73fb2b2
Gan <1354410847@qq.com>
2022-05-25 21:48:19 +0800

    [3] 看懂题目很关键

---
+++ 400-500/lc467_findSubstringInWraproundString.go
@@ -0,0 +1,28 @@
+package main
+
+func findSubstringInWraproundString(p string) (ans int) {
+	var dp [26]int
+	k := 0
+	for i, ch := range p {
+		// ab, b-a =   1 % 26 = 1
+		// za, a-z = -25 % 26 = 1
+		if i > 0 && (byte(ch)-p[i-1]+26)%26 == 1 { // 字符之差为 1 或 -25
+			k++
+		} else {
+			// 不是连续的, 重置长度从头计数
+			k = 1
+		}
+		dp[ch-'a'] = max(dp[ch-'a'], k)
+	}
+	for _, v := range dp {
+		ans += v
+	}
+	return
+}
+
+func max(a, b int) int {
+	if b > a {
+		return b
+	}
+	return a
+}
