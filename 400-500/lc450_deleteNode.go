80fac21fd4406df2935044577b4bdadd8dd92464
Gan <1354410847@qq.com>
2022-06-02 01:11:14 +0800

    [3] 二叉树的删除

--- 400-500/lc450_deleteNode.go
+++ 400-500/lc450_deleteNode.go
@@ -1,3 +1,5 @@
+//go:build ignore
+
 package main
 
 import . "leetgo/data"
@@ -44,6 +46,33 @@ func delMinNode(root *TreeNode) *TreeNode {
 	}
 	// 递归的将找没有左节点的左子树
 	root.Left = delMinNode(root.Left)
+	return root
+}
+
+func deleteNode2(root *TreeNode, key int) *TreeNode {
+	switch {
+	case root == nil:
+		return nil
+	case root.Val > key:
+		root.Left = deleteNode(root.Left, key)
+	case root.Val < key:
+		root.Right = deleteNode(root.Right, key)
+	case root.Left == nil || root.Right == nil:
+		// 找到节点了, 此时左右子节点有一个是空的, 返回非空的那一个
+		if root.Left != nil {
+			return root.Left
+		}
+		return root.Right
+	default:
+		// 左右子节点都不是空的, 这里寻找的是后继节点
+		successor := root.Right
+		for successor.Left != nil {
+			successor = successor.Left
+		}
+		successor.Right = deleteNode(root.Right, successor.Val)
+		successor.Left = root.Left
+		return successor
+	}
 	return root
 }
 
