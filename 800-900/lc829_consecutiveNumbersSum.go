3f8fe21ac3fa0ded84a69e0472408f92fdec9079
Gan <1354410847@qq.com>
2022-06-03 10:39:52 +0800

    [4] 好家伙, 我直接好家伙

--- 800-900/lc829_consecutiveNumbersSum.go
+++ 800-900/lc829_consecutiveNumbersSum.go
@@ -14,6 +14,24 @@ func consecutiveNumbersSum(N int) int {
 	return res
 }
 
+func consecutiveNumbersSum2(n int) int {
+	var ret int
+
+	// 查找指定的连续的数字的个数
+	// 9 = 9 = 4+5 = 2+3+4
+	// 连续1个, i = 1, n = 9, 9 % 1 == 0 -> 存在一个解. 9
+	// 连续2个, i = 2, n = 8, 8 % 2 == 0 -> 存在一个解. 4, 4+1(4和4+1中间差了1个数)
+	// 连续3个, i = 3, n = 6, 6 % 3 == 0 -> 存在一个解. 2, 2+1, 2+2(2和2+2中间差了2个数, 差值为3)
+
+	for i := 1; n > 0; n, i = n-i, i+1 {
+		if n%i == 0 {
+			ret += 1
+		}
+	}
+
+	return ret
+}
+
 func main() {
 
 }
