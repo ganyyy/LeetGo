3bbcdf0d5783ee74adae4459802520e0e89237b9
Gan <1354410847@qq.com>
2022-06-08 00:48:33 +0800

    [1] 十字相乘算斜率

---
+++ 1000-1100/lc1037_isBoomerang.go
@@ -0,0 +1,13 @@
+package main
+
+func isBoomerang(points [][]int) bool {
+	// 十字相乘算斜率
+	var x1 = points[0][0] - points[1][0]
+	var y1 = points[0][1] - points[1][1]
+
+	var x2 = points[0][0] - points[2][0]
+	var y2 = points[0][1] - points[2][1]
+
+	return x1*y2 != x2*y1
+
+}
