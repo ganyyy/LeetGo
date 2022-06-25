8b7bf8756a08e21a21d4ba14272fa70b1d4b22a6
Gan <1354410847@qq.com>
2022-06-26 01:09:39 +0800

    [2] 想不到, 我真的想不到啊

---
+++ 700-800/lc710_pick.go
@@ -0,0 +1,41 @@
+package main
+
+import "math/rand"
+
+type Solution struct {
+	b2w   map[int]int
+	bound int
+}
+
+func Constructor(n int, blacklist []int) Solution {
+	m := len(blacklist)
+	// 不排序/二分, 因为这样反而会更慢? what?
+	bound := n - m
+	black := map[int]bool{}
+	for _, b := range blacklist {
+		if b >= bound {
+			black[b] = true
+		}
+	}
+
+	b2w := make(map[int]int, m-len(black))
+	w := bound
+	for _, b := range blacklist {
+		if b < bound {
+			for black[w] {
+				w++
+			}
+			b2w[b] = w
+			w++
+		}
+	}
+	return Solution{b2w, bound}
+}
+
+func (s *Solution) Pick() int {
+	x := rand.Intn(s.bound)
+	if s.b2w[x] > 0 {
+		return s.b2w[x]
+	}
+	return x
+}
