5990bf04d8115bec607db0ba91504da2b5168d39
Gan <1354410847@qq.com>
2022-06-27 08:14:45 +0800

    [1] 没啥简单的方法. 先写个暴力的出来也行啊

---
+++ 500-600/lc522_findLUSlength.go
@@ -0,0 +1,30 @@
+package main
+
+func isSubseq(s, t string) bool {
+	ptS := 0
+	for ptT := range t {
+		if s[ptS] == t[ptT] {
+			if ptS++; ptS == len(s) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func findLUSlength(strs []string) int {
+	ans := -1
+next:
+	for i, s := range strs {
+		for j, t := range strs {
+			// 找到不是其他序列子集的最长序列
+			if i != j && isSubseq(s, t) {
+				continue next
+			}
+		}
+		if len(s) > ans {
+			ans = len(s)
+		}
+	}
+	return ans
+}
