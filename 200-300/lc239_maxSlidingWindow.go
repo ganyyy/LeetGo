022e6746ff2c46f96bbd1cc35e6c7a66e3b263af
Gan <1354410847@qq.com>
2022-07-18 21:55:13 +0800

    [回顾] 滑动窗口的最大值

--- 200-300/lc239_maxSlidingWindow.go
+++ 200-300/lc239_maxSlidingWindow.go
@@ -26,3 +26,31 @@ func maxSlidingWindow(nums []int, k int) []int {
 
 	return res
 }
+
+func maxSlidingWindow2(nums []int, k int) []int {
+	var queue []int
+
+	var push = func(i int) {
+		// 位置
+		// 修改成 >= 就变成了滑动窗口的最小值了...
+		for len(queue) > 0 && nums[queue[len(queue)-1]] <= nums[i] {
+			queue = queue[:len(queue)-1]
+		}
+		queue = append(queue, i)
+	}
+
+	var ret []int
+	for i := range nums {
+		push(i)
+
+		// 尝试出队
+		for len(queue) > 0 && queue[0] < i-k+1 {
+			queue = queue[1:]
+		}
+
+		if i >= k-1 {
+			ret = append(ret, nums[queue[0]])
+		}
+	}
+	return ret
+}
