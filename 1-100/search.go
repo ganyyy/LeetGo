67efc6b0696bf1be621e029e0512d219ed1fca28
Gan <1354410847@qq.com>
2022-08-23 21:38:26 +0800

    [回顾] 旋转数组?

--- 1-100/search.go
+++ 1-100/search.go
@@ -53,6 +53,52 @@ func search(nums []int, target int) int {
 	return index
 }
 
+func search33_2(nums []int, target int) int {
+	// 怎么找到旋转点呢?
+
+	// 肯定还是二分
+	// 假设中点在 旋转点的左侧
+	// 4,5,6,7,0,1,2
+	// 中点为7, 大于左侧和右侧. 如果查找的数字小于中点, 且大于左侧, 则向左靠;
+	//                        如果查找的数字大于中点, 则向右靠
+	// 假设中点在 旋转点的右侧
+	// 6,7,0,1,2,4,5
+	// 中点为1, 小于左侧和右侧. 如果查找的数字小于中点, 那么就往左靠
+	//                        如果查找的数字大于中点, 且小于右侧, 则向右靠
+
+	var l, r = 0, len(nums)
+
+	for l < r {
+		mid := l + (r-l)/2
+		mv := nums[mid]
+		if mv == target {
+			return mid
+		}
+
+		lv := nums[l]
+		rv := nums[r-1]
+
+		// fmt.Printf("[%d]%d, [%d]%d, [%d][%d]\n", l,lv,r,rv,mid,mv)
+
+		if mv > lv && mv > rv {
+			// 拐点在右侧
+			if target < mv && target >= lv {
+				r = mid
+			} else {
+				l = mid + 1
+			}
+		} else {
+			// 拐点在右侧
+			if target > mv && target <= rv {
+				l = mid + 1
+			} else {
+				r = mid
+			}
+		}
+	}
+	return -1
+}
+
 func main() {
 	var nums = []int{2, 4, 5, 6, 7, 0, 1}
 	fmt.Println(search(nums, 7))
