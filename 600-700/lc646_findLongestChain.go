55766918cd686e2583a8b6ecb592b6da45d17b4c
Gan <1354410847@qq.com>
2022-09-03 12:25:31 +0800

    [1] 贪心

---
+++ 600-700/lc646_findLongestChain.go
@@ -0,0 +1,25 @@
+package main
+
+import "sort"
+
+func findLongestChain(pairs [][]int) int {
+	// 又有点像DP?
+
+	// 为啥排第二项呢?
+	// (a, b), (c, b)
+	// 因为前置要求是a<b, c<d
+	sort.Slice(pairs, func(i, j int) bool {
+		return pairs[i][1] < pairs[j][1]
+	})
+
+	var ret = 1
+	var pre = pairs[0]
+	for i := 1; i < len(pairs); i++ {
+		if pairs[i][0] <= pre[1] {
+			continue
+		}
+		ret++
+		pre = pairs[i]
+	}
+	return ret
+}
