af52d63790d68d7c2eae0d635415c3a17010173c
Gan <1354410847@qq.com>
2022-10-21 00:28:13 +0800

    [2] 单调栈

---
+++ 900-1000/lc901_StockSpanner.go
@@ -0,0 +1,25 @@
+//go:build ignore
+
+package main
+
+import "math"
+
+type StockSpanner struct {
+	stack [][2]int // 索引和价格
+	idx   int
+}
+
+func Constructor() StockSpanner {
+	return StockSpanner{[][2]int{{-1, math.MaxInt32}}, -1}
+}
+
+func (s *StockSpanner) Next(price int) int {
+	s.idx++
+	// 单调递减栈
+	// 移除的都是连续的, 且小于当前价格的数据
+	for price >= s.stack[len(s.stack)-1][1] {
+		s.stack = s.stack[:len(s.stack)-1]
+	}
+	s.stack = append(s.stack, [2]int{s.idx, price})
+	return s.idx - s.stack[len(s.stack)-2][0]
+}
