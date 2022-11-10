80ac2025149aa47a167f4b6358d6df8c2fd7e4dc
Gan <1354410847@qq.com>
2022-11-10 23:33:00 +0800

    [5] 值二分+DFS, 优化DFS

--- 1700-1800/lc1723_minimumTimeRequired.go
+++ 1700-1800/lc1723_minimumTimeRequired.go
@@ -1,6 +1,9 @@
 package main
 
-import "sort"
+import (
+	"math"
+	"sort"
+)
 
 func minimumTimeRequired(jobs []int, k int) int {
 	n := len(jobs)
@@ -29,20 +32,60 @@ func minimumTimeRequired(jobs []int, k int) int {
 				if workloads[i]+cur <= limit {
 					workloads[i] += cur
 					if backtrack(idx + 1) {
+						// 减小limit
 						return true
 					}
 					workloads[i] -= cur
 				}
 				// 能到这一步, 说明此次分配的工作递归无法实现
 				// 如果当前工人未被分配工作，那么下一个工人也必然未被分配工作
-				// 或者当前工作恰能使该工人的工作量达到了上限, 说明给当前工人分配该工作无法完成
+				// 或者当前工作恰能使该工人的工作量达到了上限, 说明给当前工人分配该工作无法完成. 因为可以完成的情况下, 已经返回了.
 				// 这两种情况下我们无需尝试继续分配工作
 				if workloads[i] == 0 || workloads[i]+cur == limit {
 					break
 				}
 			}
+			// 增加limit
 			return false
 		}
 		return backtrack(0)
 	})
+}
+
+func minimumTimeRequired2(jobs []int, k int) int {
+
+	var sum = make([]int, k)
+	var ret = math.MaxInt32
+
+	var dfs func(i int, used int, m int)
+
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+
+	dfs = func(i int, used int, m int) {
+		if m >= ret {
+			return
+		}
+		if i == len(jobs) {
+			ret = m
+			return
+		}
+		if used < k {
+			sum[used] = jobs[i]
+			dfs(i+1, used+1, max(sum[used], m))
+			sum[used] -= jobs[i]
+		}
+		for j := 0; j < used; j++ {
+			sum[j] += jobs[i]
+			dfs(i+1, used, max(sum[j], m))
+			sum[j] -= jobs[i]
+		}
+	}
+
+	dfs(0, 0, 0)
+	return ret
 }
