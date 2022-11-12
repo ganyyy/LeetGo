19a589ace2869edb817dbd17e9a2553890874d3c
Gan <1354410847@qq.com>
2022-11-12 16:50:43 +0800

    [3] DP, 数学归纳法

---
+++ 700-800/lc790_numTilings.go
@@ -0,0 +1,13 @@
+package main
+
+func numTilings(n int) int {
+	if n == 1 {
+		return 1
+	}
+	f := make([]int, n+1)
+	f[0], f[1], f[2] = 1, 1, 2
+	for i := 3; i <= n; i++ {
+		f[i] = (f[i-1]*2 + f[i-3]) % (1e9 + 7)
+	}
+	return f[n]
+}
