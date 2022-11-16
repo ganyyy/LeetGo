ddda9a8b9ffb96d8a34104425afc9dd260be045e
Gan <1354410847@qq.com>
2022-11-17 00:23:47 +0800

    [2] 二分..., 字典树超时了, 日

---
+++ 700-800/lc792_numMatchingSubseq.go
@@ -0,0 +1,30 @@
+package main
+
+import "sort"
+
+func numMatchingSubseq(s string, words []string) int {
+	pos := [26][]int{}
+	// 存储每个字符对应的下标切片
+	for i, c := range s {
+		pos[c-'a'] = append(pos[c-'a'], i)
+	}
+	ans := len(words)
+	for _, w := range words {
+		if len(w) > len(s) {
+			ans--
+			continue
+		}
+		p := -1
+		for _, c := range w {
+			// 二分查找字母的位置
+			ps := pos[c-'a']
+			j := sort.SearchInts(ps, p+1)
+			if j == len(ps) {
+				ans--
+				break
+			}
+			p = ps[j]
+		}
+	}
+	return ans
+}
