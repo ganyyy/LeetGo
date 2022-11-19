40836a8ce4042e81c4390105503732f440ee35be
Gan <1354410847@qq.com>
2022-11-20 00:23:47 +0800

    [3] 模拟/DP题目

---
+++ 700-800/lc799_champagneTower.go
@@ -0,0 +1,27 @@
+package main
+
+import "math"
+
+func champagneTower(poured, queryRow, queryGlass int) float64 {
+	row := make([]float64, 1, queryRow+1)
+	row[0] = float64(poured)
+	nextRow := make([]float64, 0, queryRow+1)
+	for i := 1; i <= queryRow; i++ {
+		nextRow = nextRow[:i+1]
+		for i := range nextRow {
+			nextRow[i] = 0
+		}
+		// 逐层计算, 逢1减半
+		// 越靠近中间的杯子, 来源的水的数量就越多
+		for j, volume := range row {
+			// 当前行的杯子会分流给下一行 临近的杯子
+			if volume > 1 {
+				sub := (volume - 1) / 2
+				nextRow[j] += sub
+				nextRow[j+1] += sub
+			}
+		}
+		row, nextRow = nextRow, row
+	}
+	return math.Min(1, row[queryGlass])
+}
