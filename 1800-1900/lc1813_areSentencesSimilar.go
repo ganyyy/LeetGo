2746b9c0870c51f1a863fcdad6b1ce2ec9c9b976
Gan <1354410847@qq.com>
2023-01-16 01:33:28 +0800

    [3] 双指针

---
+++ 1800-1900/lc1813_areSentencesSimilar.go
@@ -0,0 +1,27 @@
+package main
+
+import "strings"
+
+func areSentencesSimilar(sentence1, sentence2 string) bool {
+	words1 := strings.Split(sentence1, " ")
+	words2 := strings.Split(sentence2, " ")
+	i, n := 0, len(words1)
+	j, m := 0, len(words2)
+	// 左向右
+	for i < n && i < m && words1[i] == words2[i] {
+		i++
+	}
+	// 右向左
+	for j < n-i && j < m-i && words1[n-j-1] == words2[m-j-1] {
+		j++
+	}
+	// 最长相似的字符串长度加一块等同于最短的那个句子
+	return i+j == min(n, m)
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
