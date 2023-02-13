3480244d3cdc1ef1c263a1bf10d8b5d0593857b5
Gan <1354410847@qq.com>
2023-02-13 22:38:56 +0800

    [3] 窗口, 但是没读懂题目

---
+++ 1200-1300/lc1234_balancedString.go
@@ -0,0 +1,43 @@
+package main
+
+func balancedString(s string) int {
+	var cnt [128]int
+	for _, c := range s {
+		cnt[byte(c)]++
+	}
+	partial := len(s) / 4
+	check := func() bool {
+		return !(cnt['Q'] > partial ||
+			cnt['W'] > partial ||
+			cnt['E'] > partial ||
+			cnt['R'] > partial)
+	}
+
+	if check() {
+		return 0
+	}
+
+	// 最长的结果, 就是整串替换
+	res := len(s)
+	r := 0
+	for l, c := range s {
+		// 右++, 直到不满足
+		for r < len(s) && !check() {
+			cnt[s[r]]--
+			r += 1
+		}
+		if !check() {
+			break
+		}
+		res = min(res, r-l)
+		cnt[byte(c)]++
+	}
+	return res
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
