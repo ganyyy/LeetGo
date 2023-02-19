2bb1958ca69e4eb5509279d4c0184188402cc306
Gan <1354410847@qq.com>
2023-02-19 14:43:55 +0800

    [2] 堆, 关于排序的机制值得关注

---
+++ 1700-1800/lc1792_maxAverageRatio.go
@@ -0,0 +1,31 @@
+package main
+
+import "container/heap"
+
+func maxAverageRatio(classes [][]int, extraStudents int) (ans float64) {
+	h := hp(classes)
+	heap.Init(&h)
+	for ; extraStudents > 0; extraStudents-- {
+		h[0][0]++
+		h[0][1]++
+		heap.Fix(&h, 0)
+	}
+	for _, c := range h {
+		ans += float64(c[0]) / float64(c[1])
+	}
+	return ans / float64(len(classes))
+}
+
+type hp [][]int
+
+func (h hp) Len() int { return len(h) }
+
+// 应该对比增加后收益变化最大的那一方(涉及到因式分解, 这里不做赘述)
+
+func (h hp) Less(i, j int) bool {
+	a, b := h[i], h[j]
+	return (a[1]-a[0])*b[1]*(b[1]+1) > (b[1]-b[0])*a[1]*(a[1]+1)
+}
+func (h hp) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (hp) Push(interface{})     {}
+func (hp) Pop() (_ interface{}) { return }
