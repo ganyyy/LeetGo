cc8bc4b82b41127c511b32f41854edc40627c083
Gan <1354410847@qq.com>
2023-03-07 00:55:05 +0800

    [4] BFP

---
+++ 1000-1100/lc1096_braceExpansionII.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"sort"
+	"strings"
+)
+
+func braceExpansionII(expression string) []string {
+	var queue []string
+	queue = append(queue, expression)
+
+	var set = make(map[string]struct{})
+	var sb strings.Builder
+
+	for len(queue) != 0 {
+		exp := queue[0]
+		queue = queue[1:]
+
+		if strings.Index(exp, "{") == -1 {
+			set[exp] = struct{}{}
+			continue
+		}
+
+		// 计算第一对括号
+		var start, end int
+		// {a,{a,b,c},c} -> {a,b,c}
+		for i, c := range exp {
+			if c == '}' {
+				end = i
+				break
+			}
+			if c == '{' {
+				start = i
+			}
+		}
+
+		before := exp[:start]
+		after := exp[end+1:]
+
+		// 这一对括号中的所有字母 {a,b,c} -> [a, b, c]
+		for _, sub := range strings.Split(exp[start+1:end], ",") {
+			sb.Reset()
+			sb.WriteString(before)
+			sb.WriteString(sub)
+			sb.WriteString(after)
+			queue = append(queue, sb.String())
+		}
+	}
+
+	var ret = make([]string, 0, len(set))
+	for sub := range set {
+		ret = append(ret, sub)
+	}
+	sort.Strings(ret)
+	return ret
+}
