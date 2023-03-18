cc0694ec1100687a45adc942b829fd3b7ac73c4f
Gan <1354410847@qq.com>
2023-03-18 16:27:04 +0800

    [2] 双指针

---
+++ 1600-1700/lc1616_checkPalindromeFormation.go
@@ -0,0 +1,30 @@
+package main
+
+func checkPalindromeFormation(a, b string) bool {
+	return checkConcatenation(a, b) || checkConcatenation(b, a)
+}
+
+func checkConcatenation(a, b string) bool {
+	left, right := 0, len(a)-1
+	for left < right && a[left] == b[right] {
+		left++
+		right--
+	}
+	if left >= right {
+		return true
+	}
+	// 比如吧
+	// abcM......
+	// ......Ncba
+	// 此时, 只需要 a中的 M... 是一个回文, 或者 b中的 ...N是一个回文 就行了
+	return checkSelfPalindrome(a[left:right+1]) || checkSelfPalindrome(b[left:right+1])
+}
+
+func checkSelfPalindrome(s string) bool {
+	left, right := 0, len(s)-1
+	for left < right && s[left] == s[right] {
+		left++
+		right--
+	}
+	return left >= right
+}
