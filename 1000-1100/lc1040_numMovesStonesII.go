05ac5e6acd3e0fdbe96a3b0bb5b35603954addc2
Gan <1354410847@qq.com>
2023-04-09 20:00:44 +0800

    [每周回顾] 区间DP, 回溯

--- 1000-1100/lc1040_numMovesStonesII.go
+++ 1000-1100/lc1040_numMovesStonesII.go
@@ -6,6 +6,13 @@ func numMovesStonesII(s []int) []int {
 	sort.Ints(s)
 	n := len(s)
 	// 最大的移动次数, 等同于 所有的空位数
+	// 简单而言, 就是保证空位最多, 然后依次走一边
+	/*
+		    0     3   5   7       11
+			X _ _ X _ X _ X _ _ _ X
+			|-------------|
+				  |---------------|
+	*/
 	e1 := s[n-2] - s[0] - n + 2
 	e2 := s[n-1] - s[1] - n + 2 // 计算空位
 	maxMove := max(e1, e2)
@@ -14,11 +21,21 @@ func numMovesStonesII(s []int) []int {
 	}
 	// 最小的移动次数
 	// 长度为n的窗口内, 最多的石子个数
+	// 只要窗口内石子最多, 说明移动的次数越少
+
+	/*
+		    0     3   5   7       11
+			X _ _ X _ X _ X _ _ _ X
+		         |_ _ _ _ _|
+	*/
 	maxCnt, left := 0, 0
 	for right, x := range s { // 滑动窗口：枚举右端点
+		// x是右边界, x-n就是左边界
 		for s[left] <= x-n { // 窗口大小大于 n
 			left++
 		}
+		// right-left 就是 长度为 n 区间内的窗口的大小
+		// 为啥是下标呢? 下标之间的差值就是 石子的个数, 然后 n-最大石子个数 = 最少的空位数
 		maxCnt = max(maxCnt, right-left+1) // 维护窗口内的最大石子数
 	}
 	return []int{n - maxCnt, maxMove}
--- 1100-1200/lc1125_smallestSufficientTeam.go
+++ 1100-1200/lc1125_smallestSufficientTeam.go
@@ -6,11 +6,15 @@ func smallestSufficientTeam(reqSkills []string, people [][]string) []int {
 	for i, skill := range reqSkills {
 		skillIndex[skill] = i
 	}
+	// 每一个状态的初始情况, 默认需要所有的员工填充
+	// 然后在DP过程中, 逐步减少所需要的员工的数量
+	// dp[i]: 技能组合到达 i 最少需要的员工数
 	dp := make([]int, 1<<n)
 	for i := range dp {
 		dp[i] = m
 	}
 	dp[0] = 0
+	// DP的核心点, 是通过
 	// 前一个状态所掌握的技能, 可以用于回溯
 	prevSkill := make([]int, 1<<n)
 	// 前置员工, 也可以用来回溯
@@ -22,10 +26,10 @@ func smallestSufficientTeam(reqSkills []string, people [][]string) []int {
 		for _, s := range people[i] {
 			curSkill |= 1 << skillIndex[s]
 		}
+		// 针对每个员工, 判断他们到指定技能组合的最小员工数
 		for prev := 0; prev < (1 << n); prev++ {
 			comb := prev | curSkill
-			// 这里就是类似于DP, 最起码需要保证不能低于前置技能
-			// > 1 说明结合这名员工的技能后, 拥有了新的进展
+			// 这里相当于要找到一个较小值
 			if dp[comb] > dp[prev]+1 {
 				dp[comb] = dp[prev] + 1
 				prevSkill[comb] = prev
@@ -33,8 +37,9 @@ func smallestSufficientTeam(reqSkills []string, people [][]string) []int {
 			}
 		}
 	}
-	res := []int{}
+	var res []int
 	i := (1 << n) - 1
+	// 从末尾状态回溯, 获取最少需要的员工编号
 	for i > 0 {
 		res = append(res, prevPeople[i])
 		i = prevSkill[i]
