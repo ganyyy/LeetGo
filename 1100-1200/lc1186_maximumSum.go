840a76cdb82433188589630d4a4a72762e9e8b0f
Gan <1354410847@qq.com>
2023-06-27 01:10:12 +0800

    [2] DP, 但是还是CV

---
+++ 1100-1200/lc1186_maximumSum.go
@@ -0,0 +1,23 @@
+//go:build ignore
+
+package main
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func maximumSum(arr []int) int {
+	// dp0: 不删除任何元素的arr[:i+1]中最大的子数组和
+	//      当前元素不删除, 可以采用 max(dp0, 0) + arr[i]
+	// dp1:   删除一次元素的arr[:i+1]中最大的子数组和
+	//      可以删arr[i], 也可以不删除arr[i]而删除前边的某个值 max(dp0, dp1+arr[i])
+	dp0, dp1, res := arr[0], 0, arr[0]
+	for i := 1; i < len(arr); i++ {
+		dp0, dp1 = max(dp0, 0)+arr[i], max(dp1+arr[i], dp0)
+		res = max(res, max(dp0, dp1))
+	}
+	return res
+}
