15234092a466432620c9d1bdc6b1e65d16e43e2b
Gan <1354410847@qq.com>
2023-11-06 07:22:26 +0800

    [2] 有点进步, 但是不多

--- 300-400/lc318_maxProduct.go
+++ 300-400/lc318_maxProduct.go
@@ -13,18 +13,39 @@ func maxProduct(words []string) int {
 
 	var ret int
 
-	var max = func(a, b int) int {
-		if a > b {
-			return a
-		}
-		return b
-	}
 	for i := 0; i < len(words)-1; i++ {
 		for j := i + 1; j < len(words); j++ {
 			if set[i]&set[j] != 0 {
 				continue
 			}
 			ret = max(ret, len(words[i])*len(words[j]))
+		}
+	}
+	return ret
+}
+
+var buf [1001]int
+
+func maxProduct2(words []string) int {
+	var pack = func(s string) int {
+		var ret int
+		for i := range s {
+			ret |= 1 << int(s[i]-'a')
+		}
+		return ret
+	}
+
+	strEncode := buf[:len(words)]
+
+	var ret int
+	for i, word := range words {
+		ei := pack(word)
+		strEncode[i] = ei
+		for j, preWord := range words[:i] {
+			if ei&strEncode[j] != 0 {
+				continue
+			}
+			ret = max(ret, len(word)*len(preWord))
 		}
 	}
 	return ret
