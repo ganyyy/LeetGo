332b7ca63b5c5e2f4436dfe494b04c603d223335
Gan <1354410847@qq.com>
2023-11-11 07:36:15 +0800

    [3] 并查集, 但是双十一

--- 700-800/lc765_minSwapsCouples.go
+++ 700-800/lc765_minSwapsCouples.go
@@ -11,7 +11,7 @@ func minSwapsCouples(row []int) int {
 
 	var cnt int
 	for i := 0; i < len(row); i += 2 {
-		var f = friend(row[i])
+		var f = row[i] ^ 1
 		if f != row[i+1] {
 			var old = row[i+1]
 			// 交换值
@@ -79,4 +79,42 @@ func minSwapsCouples3(row []int) int {
 
 func friend(a int) int {
 	return a ^ 1
+}
+
+func minSwapsCouples4(row []int) int {
+	var ln = len(row)
+	var fa = make([]int, ln>>1)
+	// 初始化一下, 每一对的父节点指向自己
+	for i := range fa {
+		fa[i] = i
+	}
+
+	var find func(i int) int
+	find = func(i int) int {
+		if i != fa[i] {
+			fa[i] = find(fa[i])
+		}
+		return fa[i]
+	}
+
+	// 咋越来越不会了..?
+
+	// 简单而言, 就是所有有交换需求的对作为一个连通分量, 组内交换 g-1 次一定满足需求(别管怎么换)
+	// 然后就是求有几个这样的连通分量
+	// 注意: 统计的时候, 两两看. 只要两个人的组不一致, 就需要连接起来. 每次连接都算是一次交换
+
+	var cnt int
+	for i := 0; i < ln>>1; i++ {
+		var a, b = find(row[i<<1] >> 1), find(row[(i<<1)+1] >> 1)
+		if a != b {
+			// 这一对不是情侣, 需要互换一下
+			if b < a {
+				a, b = b, a
+			}
+			fa[a] = b
+			cnt++
+		}
+	}
+
+	return cnt
 }
