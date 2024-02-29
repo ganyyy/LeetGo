5f2891c8643b5f9cf134243e2e15668ede93cbf1
Gan <1354410847@qq.com>
2024-03-01 00:34:04 +0800

    [3] DP?

---
+++ 2300-2400/lc2369_validPartition.go
@@ -0,0 +1,15 @@
+package main
+
+func validPartition(nums []int) bool {
+	n := len(nums)
+	f := make([]bool, n+1)
+	f[0] = true
+	for i, x := range nums {
+		if i > 0 && f[i-1] && x == nums[i-1] ||
+			i > 1 && f[i-2] && (x == nums[i-1] && x == nums[i-2] ||
+				x == nums[i-1]+1 && x == nums[i-2]+2) {
+			f[i+1] = true
+		}
+	}
+	return f[n]
+}
