85b0028ee624d5a163539c4bc3224c4ea1eef1b7
Gan <1354410847@qq.com>
2024-06-17 00:40:04 +0800

    [3] 纯暴力匹配

--- 500-600/lc522_findLUSlength.go
+++ 500-600/lc522_findLUSlength.go
@@ -1,6 +1,6 @@
 package main
 
-func isSubseq(s, t string) bool {
+func isSubSeq(s, t string) bool {
 	ptS := 0
 	for ptT := range t {
 		if s[ptS] == t[ptT] {
@@ -12,14 +12,14 @@ func isSubseq(s, t string) bool {
 	return false
 }
 
-func findLUSlength(strs []string) int {
+func findLUSLength522(strs []string) int {
 	ans := -1
 	// 没啥好说的, 纯暴力解法
 next:
 	for i, s := range strs {
 		for j, t := range strs {
 			// 找到不是其他序列子集的最长序列
-			if i != j && isSubseq(s, t) {
+			if i != j && isSubSeq(s, t) {
 				continue next
 			}
 		}
